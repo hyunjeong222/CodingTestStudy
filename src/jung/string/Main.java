a192349fe693532af9084b49adb5575d29086ca9
hyunjeong222 <hyenj629@naver.com>
2023-07-23 01:47:37 +0900

    Deleted files

--- src/jung/string/Main.java
+++
@@ -1,9 +0,0 @@
-package jung.string;
-public class Main {
-    public static int solution(int a, int b){
-        return a + b;
-    }
-    public static void main(String[] args) {
-        System.out.println(solution(1, 2) == 3);
-    }
-}
