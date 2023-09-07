d3476e8d7dfb8aa07ed7b7ccf1a2238fc3834339
hyunjeong222 <hyenj629@naver.com>
2023-09-08 02:28:35 +0900

    package update

---
+++ src/jung/Baekjoon/Boj_5430.java
@@ -0,0 +1,13 @@
+package src.jung.Baekjoon;
+
+/**
+ * 문제 이름(난이도) : AC(GOL5)
+ * 시간 : ms
+ * 메모리: KB
+ * 링크 : https://www.acmicpc.net/problem/5430
+ * */
+public class Boj_5430 {
+    public static void main(String[] args) {
+
+    }
+}
