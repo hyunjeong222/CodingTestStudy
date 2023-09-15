5077f3dd7fb57b180c2a3d196d534e182cf42f67
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-09-16 02:18:50 +0900

    [Silver II] Title: N과 M (11), Time: 332 ms, Memory: 101892 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static int[] combi;
+    static boolean[] checked;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        combi = new int[m];
+        checked = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == m) {
+            for (int val : combi) {
+                sb.append(val + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = 0; i < n; i++) {
+            if (checked[i]) {
+                continue;
+            } else if (before != arr[i]) {
+                combi[depth] = arr[i];
+                before = arr[i];
+                dfs(depth+1);
+            }
+        }
+    }
+}
