9d18449e970550c05296117e2b5d2928f1bc68d1
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-10-05 20:13:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 17372 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,63 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] vidited = new int[100001];
-	
-	static int N;
-	static int K;
-	
-	static Queue<Integer> q;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		if (N == K) { 
-			System.out.println(0);
-		} else {
-			bfs(N);
-		}
-		br.close();
-	}
-	
-	private static void bfs(int N) {
-		q = new LinkedList<Integer>();
-		q.offer(N);
-		
-		vidited[N] = 1;
-		
-		while (!q.isEmpty()) {
-			int location = q.poll();
-			
-			for (int i = 0; i < 3; i++) {
-				int next;
-				
-				if (i == 0) {
-					next = location - 1;
-				} else if (i == 1) {
-					next = location + 1;
-				} else {
-					next = location * 2;
-				}
-				
-				if (next == K) {
-					System.out.println(vidited[location]);
-					return;
-				}
-				
-				if (next >= 0 && next < vidited.length && vidited[next] == 0) {
-					q.offer(next);
-					vidited[next] = vidited[location] + 1;
-				}
-			}
-		}
-	}
+    static int n, k;
+    static int[] checked;
+    static int size = 100001;
+    static int ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        checked = new int[size];
+
+        if (n == k) bw.append(0 + "\n");
+        else {
+            ans = bfs(n);
+            bw.append(ans + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static int bfs(int x) {
+        Queue<Integer> que  = new LinkedList<>();
+        que.offer(x);
+        checked[x] = 1;
+
+        while (!que.isEmpty()) {
+            int location = que.poll();
+            for (int i = 0; i < 3; i++) {
+                int next;
+
+                if (i == 0) next = location - 1;
+                else if (i == 1) next = location + 1;
+                else next = location * 2;
+
+                if (next == k) return checked[location];
+
+                if (next >= 0 && next < checked.length && checked[next] == 0) {
+                    que.offer(next);
+                    checked[next] = checked[location]+1;
+                }
+            }
+        }
+        return -1;
+    }
 }
