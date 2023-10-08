249a1747214da98c8b58ae678f17e0634933827e
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-10-08 23:59:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 192 ms, Memory: 18040 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,63 +1,74 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[][] check;
-	static boolean[] visited;
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int n = scanner.nextInt();
-		int m = scanner.nextInt();
-		int v = scanner.nextInt();
-		
-		check = new int[n+1][n+1];
-		visited = new boolean[n+1];
-		
-		for (int i = 0; i < m; i++) {
-			int x = scanner.nextInt();
-			int y = scanner.nextInt();
-			
-			check[x][y] = check[y][x] = 1;
-		}
-		
-		dfs(v);
-		visited = new boolean[n+1];
-		System.out.println();
-		bfs(v);
-	}
-	
-	private static void dfs(int v) {
-		visited[v] = true;
-		System.out.print(v + " ");
-		if (v == check.length) {
-			return;
-		}
-		for (int i = 1; i < check.length; i++) {
-			if (check[v][i] == 1 && visited[i] == false) {
-				dfs(i);
-			}
-		}
-		
-	}
-	
-	private static void bfs(int v) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		queue.add(v);
-		visited[v] = true;
-		System.out.print(v + " ");
-		
-		while (!queue.isEmpty()) {
-			int temp = queue.poll();
-			for (int i = 1; i < check.length; i++) {
-				if (check[temp][i] == 1 && visited[i] == false) {
-					queue.add(i);
-					visited[i] = true;
-					System.out.print(i + " ");
-				}
-			}
-		}
-	}
+    static int n, m, v;
+    static int a, b;
+    static ArrayList<ArrayList<Integer>> list;
+    static boolean[] checked;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken()); // 정점의 개수
+        m = Integer.parseInt(st.nextToken()); // 간선의 개수
+        v = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호
+
+        checked = new boolean[n+1];
+
+        list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        // 방문할 수 있는 정점이 여러개인 경우 더 작은 정점 번호부터 방문
+        for (int i = 0; i <= n; i++) {
+            Collections.sort(list.get(i));
+        }
+
+        dfs(v);
+        System.out.println(sb);
+
+        sb = new StringBuilder();
+        checked = new boolean[n+1]; // bfs 하기위해 방문상태 초기화
+        bfs(v);
+        System.out.println(sb);
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(v);
+        checked[v] = true;
+
+        while (!que.isEmpty()) {
+            int now = que.poll();
+            sb.append(now + " ");
+            for (int next : list.get(now)) {
+                if (!checked[next]) {
+                    checked[next] = true;
+                    que.offer(next);
+                }
+            }
+        }
+    }
+
+    private static void dfs(int start) {
+        checked[start] = true;
+        sb.append(start + " ");
+        for (int next : list.get(start)) {
+            if (!checked[next]) {
+                dfs(next);
+            }
+        }
+    }
 }
