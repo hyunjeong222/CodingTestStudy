86eb38239a63efc159dc7478845cb597e4df53d3
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-11-01 13:33:06 +0900

    [Silver V] Title: 배열 합치기, Time: 1224 ms, Memory: 327944 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,16 +1,14 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()); // 배열 a 크기
         int m = Integer.parseInt(st.nextToken()); // 배열 b 크기
 
-        int[]  a = new int[n];
+        int[] a = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
@@ -21,21 +19,29 @@ public class Main {
             b[i] = Integer.parseInt(st.nextToken());
         }
 
-        ArrayList<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         int p1 = 0;
         int p2 = 0;
         while (p1 < n && p2 < m) {
-            if (a[p1] < b[p2]) list.add(a[p1++]);
-            else list.add(b[p2++]);
+            if (a[p1] < b[p2]) {
+                sb.append(a[p1]).append(" ");
+                p1++;
+            } else {
+                sb.append(b[p2]).append(" ");
+                p2++;
+            }
         }
-        while (p1 < n) list.add(a[p1++]);
-        while (p2 < m) list.add(b[p2++]);
+        
+        while (p1 < n) {
+            sb.append(a[p1]).append(" ");
+            p1++;
+        }
+        while (p2 < m) {
+            sb.append(b[p2]).append(" ");
+            p2++;
+        }
 
-        for (int i = 0; i < list.size(); i++) {
-            bw.append(list.get(i) + " ");
-        }
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
         br.close();
     }
 }
