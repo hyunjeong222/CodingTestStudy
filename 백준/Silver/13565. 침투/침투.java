61878f4877548378960e12ad94828dd24d13499b
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-11-09 13:29:18 +0900

    [Silver II] Title: 침투, Time: 296 ms, Memory: 43080 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.java
+++ 백준/Silver/13565. 침투/침투.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -7,6 +9,14 @@ public class Main {
     static boolean[][] checked;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static Queue<pos> que;
+    static public class pos {
+        int x; int y;
+        public pos (int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     static boolean flag = false;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,42 +25,40 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         map = new int[m][n];
+        checked = new boolean[m][n];
+        que = new LinkedList<>();
         for (int i = 0; i < m; i++) {
             String str = br.readLine();
             for (int j = 0; j < n; j++) {
                 map[i][j] = str.charAt(j) - '0';
+                if (i == 0) que.offer(new pos(i, j));
             }
         }
-        
-        checked = new boolean[m][n];
-        // 0번째 줄부터 시작해서 마지막 줄에 가면 성공 리턴
-        for (int i = 0; i < n; i++) {
-            // 전류가 통하면 0, 아니면 1
-            if (map[0][i] == 0) {
-                dfs(0, i);
-            }
-        }
+
+        bfs();
 
         System.out.println(flag ? "YES" : "NO");
     }
 
-    private static void dfs(int x, int y) {
-        checked[x][y] = true;
+    private static void bfs() {
+        while (!que.isEmpty()) {
+            pos pq = que.poll();
+            checked[pq.x][pq.y] = true;
 
-        // 마지막줄에 도착했다면 침투 성공
-        if (x == m-1) {
-            flag = true;
-            return;
-        }
+            if (pq.x == m-1) {
+                flag = true;
+                return;
+            }
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nx = pq.x + dx[i];
+                int ny = pq.y + dy[i];
 
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                if (!checked[nx][ny] && map[nx][ny] == 0) {
-                    checked[nx][ny] = true;
-                    dfs(nx, ny);
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                    if (!checked[nx][ny] && map[nx][ny] == 0) {
+                        checked[nx][ny] = true;
+                        que.offer(new pos(nx, ny));
+                    }
                 }
             }
         }
