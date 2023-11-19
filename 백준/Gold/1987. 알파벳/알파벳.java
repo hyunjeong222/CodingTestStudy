0216673c4e18b3d774c97f8331bfd4cec01704f6
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-11-19 22:04:17 +0900

    [Gold IV] Title: 알파벳, Time: 952 ms, Memory: 12728 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -7,18 +7,17 @@ public class Main {
     static boolean[] checked;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int ans = 0; // 말이 지난 최대 칸 수
+    static int ans = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken()); // 세로
-        c = Integer.parseInt(st.nextToken()); // 가로
+
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
         map = new int[r][c];
         checked = new boolean[26];
-        // 보드 입력
         for (int i = 0; i < r; i++) {
             String str = br.readLine();
             for (int j = 0; j < c; j++) {
@@ -26,7 +25,7 @@ public class Main {
             }
         }
 
-        dfs(0, 0, 0);
+        dfs(0, 0, 1);
         bw.append(ans + "\n");
         bw.flush();
         bw.close();
@@ -34,21 +33,22 @@ public class Main {
     }
 
     private static void dfs(int x, int y, int depth) {
-        if (checked[map[x][y]]) {
-            ans = Math.max(ans, depth);
-            return;
-        } else {
-            checked[map[x][y]] = true;
-            for (int i = 0; i < 4; i++) {
-                
-                int nx = dx[i] + x;
-                int ny = dy[i] + y;
+        checked[map[x][y]] = true;
+        boolean isPossible = false;
 
-                if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+
+            if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
+                if (!checked[map[nx][ny]]) {
+                    isPossible = true;
                     dfs(nx, ny, depth+1);
+                    checked[map[nx][ny]] = false;
                 }
             }
-            checked[map[x][y]] = false;
         }
+
+        if (!isPossible) ans = Math.max(ans, depth);
     }
 }
