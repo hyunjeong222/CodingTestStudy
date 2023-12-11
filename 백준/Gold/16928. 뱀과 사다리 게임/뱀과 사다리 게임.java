c95a2e53f8dd270a538c2da39247aaf11fca93fe
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-12-11 15:59:03 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 80 ms, Memory: 11764 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -6,15 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
     static int n, m;
     static int[] map;
-    static boolean[] checked;
-    static public class pos {
-        int x;
-        int cnt;
-        public pos(int x, int cnt) {
-            this.x = x;
-            this.cnt = cnt;
-        }
-    }
+    static int[] checked;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -35,7 +27,7 @@ public class Main {
             map[u] = v;
         }
 
-        checked = new boolean[101];
+        checked = new int[101];
         int ans = bfs();
         bw.append(ans + "\n");
         bw.flush();
@@ -44,25 +36,24 @@ public class Main {
     }
 
     private static int bfs() {
-        Queue<pos> que = new LinkedList<>();
-        que.offer(new pos(1, 0));
-        checked[1] = true;
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(1);
+        checked[1] = 0;
 
-        while (!que.isEmpty()) {
-            pos cur = que.poll();
-
-            if (cur.x == 100) return cur.cnt;
+        while (true) {
+            int cur = que.poll();
 
             for (int i = 1; i < 7; i++) {
-                int next = cur.x+i;
+                int next = cur+i;
 
-                if (next > 100 || checked[next]) continue;
+                if (next > 100) continue;
 
-                que.offer(new pos(map[next], cur.cnt+1));
-                checked[next] = true;
+                if (checked[map[next]] == 0) {
+                    que.offer(map[next]);
+                    checked[map[next]] = checked[cur]+1;
+                }
+                if (map[next] == 100) return checked[100];
             }
         }
-
-        return -1;
     }
 }
