ec8794a559fc0b424ec0e9228aa7a29dae5b3429
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-12-14 01:41:51 +0900

    [Silver I] Title: 효율적인 해킹, Time: 8860 ms, Memory: 307700 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,66 +1,60 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static computer[] list;
-    static boolean[] checked;
-    static int[] result;
-    static public class computer {
-        int idx; ArrayList<computer> node;
-        public computer(int idx) {
-            this.idx = idx;
-            this.node = new ArrayList<>();
-        }
-    }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] inputs = br.readLine().split(" ");
-        n = Integer.parseInt(inputs[0]);
-        m = Integer.parseInt(inputs[1]);
-
-        list = new computer[n+1];
-        for (int i = 0; i <= n; i++) {
-            list[i] = new computer(i);
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        List<Integer>[] adjList = new List[n + 1];
+        for(int i = 1; i <= n; i++) {
+            adjList[i] = new ArrayList<>();
         }
-
-        for (int i = 0; i < m; i++) {
-            inputs = br.readLine().split(" ");
-            int a = Integer.parseInt(inputs[0]);
-            int b = Integer.parseInt(inputs[1]);
-
-            list[b].node.add(list[a]);
+        
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int to = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            adjList[from].add(to);
         }
-
-        result = new int[n+1];
-        for (int i = 1; i <= n; i++) {
-            checked = new boolean[n+1];
-            checked[i] = true;
-            dfs(i, i);
-        }
-
+        
+        int[] save = new int[n + 1];
         int max = 0;
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, result[i]);
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (result[i] == max) bw.write(i + " ");
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    private static void dfs(int start, int now) {
-        for (computer next : list[now].node) {
-            if (!checked[next.idx]) {
-                checked[next.idx] = true;
-                dfs(start, next.idx);
-                result[start]++;
+        for(int i = 1; i <= n; i++) {
+        	List<Integer> same = new ArrayList<>();
+        	same.add(i);
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(i);
+            boolean[] visited = new boolean[n + 1];
+            visited[i] = true;
+            int count = 0;
+            while(!queue.isEmpty()) {
+                int now = queue.poll();
+                count++;
+                for(int next : adjList[now]) {
+                    if(visited[next]) continue;
+                    visited[next] = true;
+                    queue.offer(next);
+                }
             }
+            save[i] = count;
+            max = Math.max(max, count);
         }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+        	if(save[i] == max) {
+        		sb.append(i).append(' ');
+        	}
+        }
+        System.out.println(sb);
     }
 }
