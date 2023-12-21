ec77e39c40e49d1f183cf1af2ef99d8ec4181eec
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2023-12-21 13:15:35 +0900

    [Gold V] Title: 두 개의 탑, Time: 1964 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static int n;
+    static int[] prefix;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        prefix = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = Integer.parseInt(br.readLine()) + prefix[i-1];
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                int rightD = prefix[j-1] - prefix[i-1];
+                int leftD = prefix[n] - rightD;
+                int min = Math.min(rightD, leftD);
+                ans = Math.max(ans, min);
+            }
+        }
+        bw.append(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
