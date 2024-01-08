2446c123da2a371af683b878b30b5bba00ed009f
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-01-08 20:45:13 +0900

    [Silver I] Title: 양치기 꿍, Time: 116 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Silver/3187. 양치기 꿍/양치기 꿍.java
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.java
@@ -1,21 +1,20 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int r, c;
-    static int sCnt = 0;
-    static int wCnt = 0;
     static char[][] map;
     static boolean[][] checked;
+    static int wCnt, sCnt;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
+        wCnt = 0; sCnt = 0;
         map = new char[r][c];
         checked = new boolean[r][c];
         for (int i = 0; i < r; i++) {
@@ -24,33 +23,32 @@ public class Main {
                 map[i][j] = str.charAt(j);
             }
         }
-        int sTotal = 0; int wTotal = 0;
+        int wTotal = 0;
+        int sTotal = 0;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (!checked[i][j] && map[i][j] != '#') {
+                if (!checked[i][j] && map[i][j] == '#') {
                     sCnt = 0; wCnt = 0;
                     dfs(i, j);
                     if (sCnt > wCnt) sTotal += sCnt;
-                    else  wTotal += wCnt;
+                    else wTotal += wCnt;
                 }
             }
         }
-        bw.append(sTotal + " " + wTotal);
-        bw.flush();
-        bw.close();
-        br.close();
+        sb.append(sTotal).append(" ").append(wTotal);
+        System.out.println(sb);
     }
 
     private static void dfs(int x, int y) {
-        if (map[x][y] == 'k') sCnt++;
-        else if (map[x][y] == 'v') wCnt++;
-
+        if (map[x][y] == 'v') wCnt++;
+        else if (map[x][y] == 'k') sCnt++;
         checked[x][y] = true;
         for (int i = 0; i < 4; i++) {
             int nx = dx[i] + x;
             int ny = dy[i] + y;
-            if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
-                if (!checked[nx][ny] && map[nx][ny] != '#') {
+
+            if (nx >= 0 && nx < r && ny >= 0 && ny < c && !checked[nx][ny]) {
+                if (map[nx][ny] != '#') {
                     dfs(nx, ny);
                 }
             }
