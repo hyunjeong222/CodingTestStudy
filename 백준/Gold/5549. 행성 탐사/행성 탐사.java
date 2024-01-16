8a6014573b34622577aa171f4faee6ca04e09f40
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-01-16 13:24:55 +0900

    [Gold V] Title: 행성 탐사, Time: 932 ms, Memory: 104832 KB -BaekjoonHub

--- 백준/Gold/5549. 행성 탐사/행성 탐사.java
+++ 백준/Gold/5549. 행성 탐사/행성 탐사.java
@@ -11,23 +11,20 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(br.readLine()); // 조사 대상 영역의 개수
-        int[][][] prefix = new int[n+1][m+1][3]; // 좌표, 정글 바다 얼음의 정보
+        int[][][] prefix = new int[n+1][m+1][3]; // 0: 정글, 1: 바다, 2: 얼음
         for (int i = 1; i <= n; i++) {
             String str = br.readLine();
             for (int j = 1; j <= m; j++) {
                 for (int l = 0; l < 3; l++) {
                     prefix[i][j][l] = prefix[i-1][j][l] + prefix[i][j-1][l] - prefix[i-1][j-1][l];
                 }
-                switch (str.charAt(j-1)) {
-                    case 'J' : // 정글
-                        prefix[i][j][0]++;
-                        break;
-                    case 'O' : // 얼음
-                        prefix[i][j][1]++;
-                        break;
-                    case 'I' : // 바다
-                        prefix[i][j][2]++;
-                        break;
+                char type = str.charAt(j-1);
+                if (type == 'J') { // 정글
+                    prefix[i][j][0]++;
+                } else if (type == 'O') { // 얼음
+                    prefix[i][j][1]++;
+                } else { // 바다
+                    prefix[i][j][2]++;
                 }
             }
         }
