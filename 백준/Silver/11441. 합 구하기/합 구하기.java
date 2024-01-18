8a7638c0388cee48d593d6042118298f1d3be1d2
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-01-18 23:15:39 +0900

    [Silver III] Title: 합 구하기, Time: 600 ms, Memory: 56460 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.java
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -4,24 +4,21 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine()); // 수의 개수
         int[] arr = new int[n+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
-            arr[i] += arr[i-1];
+            arr[i] = arr[i-1]+Integer.parseInt(st.nextToken());
         }
         int m = Integer.parseInt(br.readLine());
         while (m --> 0) {
             st = new StringTokenizer(br.readLine());
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
-            bw.append(arr[j]-arr[i-1] + "\n");
+            sb.append(arr[j]-arr[i-1]).append("\n");
         }
-        bw.flush();
-        bw.close();
         br.close();
+        System.out.println(sb);
     }
 }
