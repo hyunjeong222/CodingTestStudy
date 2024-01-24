5113e78b18fbacd3a1e90a5ad3b169fad93a493d
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-01-24 14:31:44 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 116 ms, Memory: 18052 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        long[][] dp = new long[100001][4];
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        for (int i = 4; i < dp.length; i++) {
+            dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % 1000000009;
+            dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % 1000000009;
+            dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % 1000000009;
+        }
+        while (t-->0) {
+            int n = Integer.parseInt(br.readLine());
+            long ans = (dp[n][1] + dp[n][2] + dp[n][3]) % 1000000009;
+            bw.append(ans + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
