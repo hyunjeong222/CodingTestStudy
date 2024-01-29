8c38a4959c205059c07c70ebd535f59ad3f859b5
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-01-29 22:21:20 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 600 ms, Memory: 75076 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -12,13 +12,10 @@ public class Main {
         HashMap<String, String> map = new HashMap<>();
         while (n-->0) {
             st = new StringTokenizer(br.readLine());
-            String site = st.nextToken();
-            String pw = st.nextToken();
-            map.put(site, pw);
+            map.put(st.nextToken(), st.nextToken());
         }
         while (m-->0) {
-            String site = br.readLine();
-            bw.append(map.get(site) + "\n");
+            bw.append(map.get(br.readLine()) + "\n");
         }
         bw.flush();
         bw.close();
