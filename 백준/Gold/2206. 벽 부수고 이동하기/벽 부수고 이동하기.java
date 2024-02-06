20af0a3a2f76b75974b5728a6158c34f1481a754
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-02-06 12:10:47 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 696 ms, Memory: 160608 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -6,73 +6,68 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] map;
-	static boolean[][][] visited;
-	static int n;
-	static int m;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static public class dot {
-		int x, y;
-		int count;
-		int isbreak;
-		public dot(int x, int y, int count, int isbreak) {
-			this.x = x;
-			this.y = y;
-			this.count = count;
-			this.isbreak = isbreak;
-		}
-	}
+    static int n, m;
+    static int[][] map;
+    static boolean[][][] checked;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static Queue<Pos> que;
+    static public class Pos {
+        int x; int y; int cnt; boolean flag;
+        public Pos(int x, int y, int cnt, boolean flag) {
+            this.x = x; this.y = y; this.cnt = cnt; this.flag = flag;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        checked = new boolean[n][m][2];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+        int ans = bfs(0, 0);
+        System.out.println(ans);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+    private static int bfs(int x, int y) {
+        que = new LinkedList<>();
+        que.offer(new Pos(x, y, 1, false));
+        checked[x][y][0] = true;
+        checked[x][y][1] = true;
 
-		map = new int[n][m];
-		visited = new boolean[n][m][2];
+        while (!que.isEmpty()) {
+            Pos now = que.poll();
 
-		for (int i = 0; i < n; i++) {
-			String[] str = br.readLine().split("");
-			for (int j = 0; j < m; j++) {
-				map[i][j] = Integer.parseInt(str[j]);
-			}
-		}
-		
-		System.out.println(bfs(0,0));
-	}
+            if (now.x == n-1 && now.y == m-1) return now.cnt;
 
-	private static int bfs(int x, int y) {
-		Queue<dot> q = new LinkedList<dot>();
-		q.offer(new dot(x, y, 1, 0));
-		visited[x][y][0] = true;
-		visited[x][y][1] = true;
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + now.x;
+                int ny = dy[i] + now.y;
 
-		while (!q.isEmpty()) {
-			dot d_q = q.poll();
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 
-			if (d_q.x == n - 1 && d_q.y == m - 1) return d_q.count;
-
-			for (int i = 0; i < 4; i++) {
-				int nx = d_q.x + dx[i];
-				int ny = d_q.y + dy[i];
-
-				if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-					if (map[nx][ny] == 0) {
-						if (!visited[nx][ny][d_q.isbreak]) {
-							q.offer(new dot(nx, ny, d_q.count+1, d_q.isbreak));
-							visited[nx][ny][d_q.isbreak] = true;
-						}
-					} else if (map[nx][ny] == 1) {
-						if (d_q.isbreak == 0 && !visited[nx][ny][1]) {
-							q.offer(new dot(nx, ny, d_q.count+1, 1));
-							visited[nx][ny][1] = true;
-						}
-					}
-				}
-			}
-		}
-		return -1;
-	}
+                if (map[nx][ny] == 0) {
+                    if (!now.flag && !checked[nx][ny][0]) {
+                        que.offer(new Pos(nx, ny, now.cnt+1, false));
+                        checked[nx][ny][0] = true;
+                    } else if (now.flag && !checked[nx][ny][1]) {
+                        que.offer(new Pos(nx, ny, now.cnt+1, true));
+                        checked[nx][ny][1] = true;
+                    }
+                } else {
+                    if (!now.flag) {
+                        que.offer(new Pos(nx, ny, now.cnt+1, true));
+                        checked[nx][ny][1] = true;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
 }
