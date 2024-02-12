b9c4da7d483c2c21b22c073ae6d0d24d4d6bbdb9
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-02-12 23:48:23 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 740 ms, Memory: 82108 KB -BaekjoonHub

---
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static ArrayList<ArrayList<Integer>> list;
+    static boolean[] checked;
+    static Queue<Integer> que;
+    static int[] ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        checked = new boolean[n+1];
+        StringTokenizer st;
+        list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n-1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+        ans = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            ans[i] = Integer.parseInt(st.nextToken());
+        }
+        if (bfs(1)) System.out.println(1);
+        else System.out.println(0);
+    }
+
+    private static boolean bfs(int x) {
+        que = new LinkedList<>();
+        que.offer(x);
+        checked[x] = true;
+        HashSet<Integer> set = new HashSet<>();
+        int idx = 1;
+        while (!que.isEmpty()) {
+            set.clear();
+            int now = que.poll();
+            for (int y : list.get(now)) {
+                if (!checked[y]) {
+                    set.add(y);
+                    checked[y] = true;
+                }
+            }
+            int size = set.size();
+            for (int i = idx; i < idx + size; i++) {
+                if (set.contains(ans[i])) que.offer(ans[i]);
+                else return false;
+            }
+            idx += size;
+        }
+        return true;
+    }
+}
