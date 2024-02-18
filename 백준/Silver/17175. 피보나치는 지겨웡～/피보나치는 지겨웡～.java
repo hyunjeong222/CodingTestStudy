c3d186e73b64c7ee89fb8f1d8420cda61e20d704
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-02-18 20:20:06 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[51];
+        dp[0] = 1; dp[1] = 1;
+        for (int i = 2; i <= 50; i++) {
+            dp[i] = (dp[i-2] + dp[i-1] + 1) % 1000000007;
+        }
+        System.out.println(dp[n]%1000000007);
+    }
+}
