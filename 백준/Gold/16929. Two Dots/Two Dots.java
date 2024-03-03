feaaf6e6dd8666dd0f6543e5112bc9ecaaf7e156
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-03-03 11:00:18 +0900

    [Gold IV] Title: Two Dots, Time: 112 ms, Memory: 12988 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.java
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -8,8 +8,8 @@ public class Main {
     static int n, m;
     static char[][] map;
     static boolean[][] checked;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int startX, startY;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,38 +17,39 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new char[n][m];
-        checked = new boolean[n][m];
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
+            char[] input = br.readLine().toCharArray();
             for (int j = 0; j < m; j++) {
-                map[i][j] = str.charAt(j);
+                map[i][j] = input[j];
             }
         }
+        checked = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 startX = i;
                 startY = j;
-                char color = map[i][j];
-                dfs(i, j, color, 1);
+                checked[i][j] = true;
+                dfs(i, j, 1);
+                checked[i][j] = false;
             }
         }
         System.out.println("No");
     }
 
-    private static void dfs(int x, int y, char color, int cnt) {
-        if (cnt >= 4 && x == startX && y == startY) {
-            System.out.println("Yes");
-            System.exit(0);
-        }
-
+    private static void dfs(int x, int y, int cnt) {
         for (int i = 0; i < 4; i++) {
             int nx = dx[i] + x;
             int ny = dy[i] + y;
 
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || checked[nx][ny] || color != map[nx][ny]) continue;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if (nx == startX && ny == startY && cnt >= 4) {
+                System.out.println("Yes");
+                System.exit(0);
+            }
+            if(checked[nx][ny] || map[x][y] != map[nx][ny]) continue;
 
             checked[nx][ny] = true;
-            dfs(nx, ny, color, cnt+1);
+            dfs(nx, ny, cnt+1);
             checked[nx][ny] = false;
         }
     }
