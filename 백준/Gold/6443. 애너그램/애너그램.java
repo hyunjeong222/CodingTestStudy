f1d22e53019375d42d11ddb006719bdb48c7b218
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-03-07 20:28:52 +0900

    [Gold V] Title: 애너그램, Time: 484 ms, Memory: 53372 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -6,20 +6,19 @@ import java.util.*;
 public class Main {
     static int n;
     static int[] alpha;
-    static Stack<Character> stack = new Stack<>();
+    static StringBuilder sb = new StringBuilder();
     static PriorityQueue<String> pq = new PriorityQueue<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         String str;
-        StringBuilder sb = new StringBuilder();
         while (n --> 0) {
             str = br.readLine();
             alpha = new int[26];
             for (int i = 0; i < str.length(); i++) {
                 alpha[str.charAt(i) - 'a']++;
             }
-            dfs(str.length());
+            dfs(0, str.length(), new StringBuilder());
             while (!pq.isEmpty()) {
                 sb.append(pq.poll()).append("\n");
             }
@@ -27,23 +26,19 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static void dfs(int len) {
-        if (len == stack.size()) {
-            StringBuilder sb = new StringBuilder();
-            for (char now : stack) {
-                sb.append(now);
-            }
-            pq.offer(sb.toString());
+    private static void dfs(int depth, int len, StringBuilder now) {
+        if (depth == len) {
+            pq.offer(now.toString());
+            return;
         }
 
         for (int i = 0; i < alpha.length; i++) {
-            if (alpha[i] > 0) {
-                alpha[i]--;
-                stack.push((char)(i+'a'));
-                dfs(len);
-                stack.pop();
-                alpha[i]++;
-            }
+            if (alpha[i] < 1) continue;
+            alpha[i]--;
+            now.append((char)(i+'a'));
+            dfs(depth+1, len, now);
+            now.deleteCharAt(now.length()-1);
+            alpha[i]++;
         }
     }
 }
