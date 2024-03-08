657afce713fb1c3a17b2af11573fba696c2138a7
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-03-08 13:57:05 +0900

    [Gold III] Title: 윤이는 엄청난 것을 훔쳐갔습니다, Time: 1168 ms, Memory: 140180 KB -BaekjoonHub

--- 백준/Gold/27924. 윤이는 엄청난 것을 훔쳐갔습니다/윤이는 엄청난 것을 훔쳐갔습니다.java
+++ 백준/Gold/27924. 윤이는 엄청난 것을 훔쳐갔습니다/윤이는 엄청난 것을 훔쳐갔습니다.java
@@ -1,19 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] list;
-    static boolean[] leaf;
+    static ArrayList<Integer>[] graph;
     static int[][] dist;
+    static public class Pos {
+        int node; int dist;
+        public Pos(int node, int dist) {
+            this.node = node; this.dist = dist;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        list = new ArrayList[n+1];
+        graph = new ArrayList[n+1];
         for (int i = 1; i <= n; i++) {
-            list[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
         StringTokenizer st;
         int u, v;
@@ -21,37 +25,41 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             u = Integer.parseInt(st.nextToken());
             v = Integer.parseInt(st.nextToken());
-            list[u].add(v);
-            list[v].add(u);
-        }
-        leaf = new boolean[n+1];
-        for (int i = 1; i <= n; i++) {
-            if (list[i].size() == 1) {
-                leaf[i] = true;
-            }
+            graph[u].add(v);
+            graph[v].add(u);
         }
         dist = new int[3][n+1];
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        dfs(0, a, 1);
-        dfs(1, b, 1);
-        dfs(2, c, 1);
+        bfs(0, a); // 윤
+        bfs(1, b); // 달구
+        bfs(2, c); // 포닉스
 
         for (int i = 1; i <= n; i++) {
-            if (leaf[i] && dist[0][i] < dist[1][i] && dist[0][i] < dist[2][i]) {
+            if (graph[i].size() == 1 && dist[0][i] < dist[1][i] && dist[0][i] < dist[2][i]) {
                 System.out.println("YES");
                 System.exit(0);
             }
         }
         System.out.println("NO");
     }
-    private static void dfs(int o, int now, int dis) {
-        dist[o][now] = dis;
-        for (int i : list[now]) {
-            if (dist[o][i] == 0) {
-                dfs(o, i, dis+1);
+    private static void bfs(int idx, int node) {
+        boolean[] checked = new boolean[graph.length];
+        Queue<Pos> que = new LinkedList<>();
+        que.offer(new Pos(node, 0));
+        checked[node] = true;
+        while (!que.isEmpty()) {
+            Pos now = que.poll();
+            int num = now.node; int dst = now.dist;
+            dist[idx][num] = dst;
+
+            for (int next : graph[num]) {
+                if (!checked[next]) {
+                    checked[next] = true;
+                    que.offer(new Pos(next, dst+1));
+                }
             }
         }
     }
