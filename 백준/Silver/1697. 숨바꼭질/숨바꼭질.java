21c29a78e940d91469a6dc22b531bfc128f4f3b4
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-03-22 01:10:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 16368 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,55 +1,64 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, k;
-    static int[] checked;
-    static int size = 100001;
-    static int ans;
+    static boolean[] checked;
+    static Queue<Pos> que;
+    static public class Pos {
+        int x; int time;
+        public Pos(int x, int time) {
+            this.x = x; this.time = time;
+        }
+    }
+    static int ans = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        checked = new int[size];
-
-        if (n == k) bw.append(0 + "\n");
-        else {
-            ans = bfs(n);
-            bw.append(ans + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
+        n = Integer.parseInt(st.nextToken()); // 수빈이 위치
+        k = Integer.parseInt(st.nextToken()); // 동생의 위치
+        checked = new boolean[100001];
+        que = new LinkedList<>();
+        que.offer(new Pos(n, 0));
+        bfs();
+        System.out.println(ans);
     }
 
-    private static int bfs(int x) {
-        Queue<Integer> que  = new LinkedList<>();
-        que.offer(x);
-        checked[x] = 1;
-
+    private static void bfs() {
         while (!que.isEmpty()) {
-            int location = que.poll();
-            for (int i = 0; i < 3; i++) {
-                int next;
+            Pos now = que.poll();
+            int cur = now.x;
+            int time = now.time;
 
-                if (i == 0) next = location - 1;
-                else if (i == 1) next = location + 1;
-                else next = location * 2;
+            if (cur == k) {
+                ans = time;
+                return;
+            }
 
-                if (next == k) return checked[location];
+            checked[cur] = true;
 
-                if (next >= 0 && next < checked.length && checked[next] == 0) {
-                    que.offer(next);
-                    checked[next] = checked[location]+1;
-                }
+            if (check(cur+1) && !checked[cur+1]) {
+                checked[cur+1] = true;
+                que.offer(new Pos(cur+1, time+1));
+            }
+
+            if (check(cur-1) && !checked[cur-1]) {
+                checked[cur-1] = true;
+                que.offer(new Pos(cur-1, time+1));
+            }
+
+            if (check(cur*2) && !checked[cur*2]) {
+                checked[cur*2] = true;
+                que.offer(new Pos(cur*2, time+1));
             }
         }
-        return -1;
+    }
+
+    private static boolean check(int x) {
+        return x >= 0 && x < 100001;
     }
 }
