5cde5b84d8a20a40cd60bb7d2d42bdd0cd5b38de
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-03-24 10:48:45 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 984 ms, Memory: 93784 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, r;
+    static boolean[] checked;
+    static ArrayList<ArrayList<Integer>> list;
+    static public int[] count;
+    static int cnt = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        int u, v;
+        while (m --> 0) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+
+        for (ArrayList<Integer> l : list) {
+            Collections.sort(l, Collections.reverseOrder());
+        }
+
+        checked = new boolean[n+1];
+        count = new int[n+1];
+        dfs(r);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(count[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(int x) {
+        checked[x] = true;
+        count[x] = cnt++;
+
+        for (int next : list.get(x)) {
+            if (checked[next]) continue;
+
+            checked[next] = true;
+            dfs(next);
+        }
+    }
+}
