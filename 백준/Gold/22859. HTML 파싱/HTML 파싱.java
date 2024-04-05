3fae4ac6558243318a16ab862e631ad7eac96dae
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-04-06 00:57:19 +0900

    [Gold III] Title: HTML 파싱, Time: 728 ms, Memory: 45584 KB -BaekjoonHub

--- 백준/Gold/22859. HTML 파싱/HTML 파싱.java
+++ 백준/Gold/22859. HTML 파싱/HTML 파싱.java
@@ -1,34 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
+    static String[] html;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String html = br.readLine();
-
-        Matcher p1 = Pattern.compile("div title=\"(\\w|_|\\s)*\"").matcher(html);
-        Matcher p2 = Pattern.compile("<p>(\\w|\\s|</?[^p]>|</?\\w{2,}\\s?>|\\.)*</p>").matcher(html);
-
-        HashMap<Integer, String> map = new HashMap<>();
-        while (p1.find()) {
-            map.put(p1.start(), "title : "+p1.group().split("\"")[1]+"\n");
-        }
-        while (p2.find()) {
-            String tmp = p2.group().replaceAll("<[\\w\\s/]*>", "");
-            map.put(p2.start(), tmp.replaceAll("\\s{2,}", " ")+"\n");
-        }
-        StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list);
-        for (int i : list) {
-            sb.append(map.get(i));
-        }
+        html = br.readLine().split("<p>|</p>|</div>");
+        parsing();
         System.out.println(sb);
+    }
+
+    private static void parsing() {
+        for (String str : html) {
+            if (str.isEmpty()) continue;
+
+            if (str.contains("<div ")) {
+                String tmp = str.split("\"")[1];
+                sb.append("title : ").append(tmp).append("\n");
+            } else {
+                String tmp = str.replaceAll("<[\\w\\s/]*>", "");
+                tmp = tmp.replaceAll("\\s{2,}", " ");
+                sb.append(tmp).append("\n");
+            }
+        }
     }
 }
