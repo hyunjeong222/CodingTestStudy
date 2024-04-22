f58c966d034737bbead2913b14355eec54ee8e7c
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-04-22 19:39:33 +0900

    [Gold IV] Title: 여왕벌, Time: 3840 ms, Memory: 304020 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
     static int[][] map;
-    static int[] arr;
+    static int zero, one, two;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,23 +16,22 @@ public class Main {
 
         map = new int[m][m];
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                map[i][j] = 1;
-            }
+            Arrays.fill(map[i], 1);
         }
 
-        arr = new int[m*2-1];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int idx = 0;
-            for (int j = 0; j < 3; j++) {
-                int cnt = Integer.parseInt(st.nextToken());
-                while (cnt --> 0) {
-                    arr[idx++] = j;
-                }
-            }
-
+            zero = Integer.parseInt(st.nextToken());
+            one = Integer.parseInt(st.nextToken());
+            two = Integer.parseInt(st.nextToken());
+            
             sizeUp();
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < m; j++) {
+                map[i][j] = Math.max(map[i-1][j-1], Math.max(map[i-1][j], map[i][j-1]));
+            }
         }
 
         StringBuilder sb = new StringBuilder();
@@ -45,16 +45,24 @@ public class Main {
     }
 
     private static void sizeUp() {
-        int idx = 0;
         for (int i = m-1; i > 0; i--) {
-            map[i][0] += arr[idx++];
+            if (zero != 0) zero--;
+            else if (one != 0) {
+                one--;
+                map[i][0]+=1;
+            } else {
+                two--;
+                map[i][0]+=2;
+            }
         }
         for (int i = 0; i < m; i++) {
-            map[0][i] += arr[idx++];
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < m; j++) {
-                map[i][j] = Math.max(map[i-1][j-1], Math.max(map[i-1][j], map[i][j-1]));
+            if (zero != 0) zero--;
+            else if (one != 0) {
+                one--;
+                map[0][i]+=1;
+            } else {
+                two--;
+                map[0][i]+=2;
             }
         }
     }
