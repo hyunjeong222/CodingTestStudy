80534146cd64e6d933f7a325e3c62839eeb5446e
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-05-10 18:15:26 +0900

    [Gold V] Title: 매직 스타, Time: 376 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Gold/3967. 매직 스타/매직 스타.java
+++ 백준/Gold/3967. 매직 스타/매직 스타.java
@@ -4,36 +4,38 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
-    static char[][] map = new char[5][9];
-    static boolean[] checked = new boolean[13];
-    static ArrayList<Pos> list = new ArrayList<>();
+    static char[][] map;
+    static boolean[] checked;
+    static ArrayList<Pos> list = new ArrayList<Pos>();
     static public class Pos {
         int x; int y;
-        public Pos(int x, int y) {
+        public Pos (int x, int y) {
             this.x = x; this.y = y;
         }
     }
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        map = new char[5][9];
+        checked = new boolean[12];
+
         for (int i = 0; i < 5; i++) {
             String str = br.readLine();
             for (int j = 0; j < 9; j++) {
                 map[i][j] = str.charAt(j);
-                if (map[i][j] == 'x') { // 채워야할 곳 리스트에 담아놓기
-                    list.add(new Pos(i, j));
-                } else if (map[i][j] != '.') { // 채워져있는 곳 방문체크
-                    checked[map[i][j]-65] = true;
-                }
+
+                if (map[i][j] == 'x') list.add(new Pos(i, j));
+                else if (map[i][j] != '.') checked[map[i][j]-65] = true;
             }
         }
 
         backtracking(0);
+
     }
 
     private static void backtracking(int depth) {
-        if (depth == list.size()) { // 매직스타를 다 채웠다면
-            if (check()) { // 모든 합이 26이 되는지 체크
+        if (depth == list.size()) {
+            if (check()) {
                 for (int i = 0; i < 5; i++) {
                     for (int j = 0; j < 9; j++) {
                         sb.append(map[i][j]);
@@ -46,8 +48,8 @@ public class Main {
             return;
         }
 
-        for (int i = 0; i < 12; i++) { // 1~12 숫자 탐색
-            if (!checked[i]) { // 쓰이지 않은 알파벳이라면
+        for (int i = 0; i < 12; i++) {
+            if (!checked[i]) {
                 Pos now = list.get(depth);
                 char alpha = (char)(65+i);
                 map[now.x][now.y] = alpha;
@@ -67,9 +69,7 @@ public class Main {
         int sum5 = (map[4][4]-'A'+1)+(map[3][3]-'A'+1)+(map[2][2]-'A'+1)+(map[1][1]-'A'+1);
         int sum6 = (map[4][4]-'A'+1)+(map[3][5]-'A'+1)+(map[2][6]-'A'+1)+(map[1][7]-'A'+1);
 
-        if (sum1==26 && sum2==26 && sum3==26 && sum4==26 && sum5==26 && sum6==26) {
-            return true;
-        }
+        if (sum1 == 26 && sum2 == 26 && sum3 == 26 && sum4 == 26 && sum5 == 26 && sum6 == 26) return true;
 
         return false;
     }
