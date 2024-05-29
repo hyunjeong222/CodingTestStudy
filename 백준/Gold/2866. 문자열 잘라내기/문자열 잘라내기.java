171f120fde975ea1f11f5a1bd5ec8ecbae9cb1cc
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-05-29 22:14:11 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 1196 ms, Memory: 304960 KB -BaekjoonHub

--- 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
@@ -1,39 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int r, c;
+    static char[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken()); // 행
-        int c = Integer.parseInt(st.nextToken()); // 열
+        r = Integer.parseInt(st.nextToken()); // 행
+        c = Integer.parseInt(st.nextToken()); // 열
 
-        String[] arr = new String[c];
+        arr = new char[r][c];
         for (int i = 0; i < r; i++) {
             String str = br.readLine();
             for (int j = 0; j < c; j++) {
-                if (i == 0) arr[j] = String.valueOf(str.charAt(j));
-                else arr[j] += String.valueOf(str.charAt(j));
-            }
-        }
-        // da, od, ba, at, ra, zk
-
-        for (int i = 0; i < r-1; i++) {
-            Set<String> set = new HashSet<>();
-
-            for (int j = 0; j < c; j++) {
-                String key = arr[j].substring(i+1);
-                if (set.contains(key)) {
-                    System.out.println(i);
-                    System.exit(0);
-                } else set.add(key);
+                arr[i][j] = str.charAt(j);
             }
         }
 
-        System.out.println(r-1);
+        int start = 0;
+        int end = r-1;
+
+        while (start <= end) {
+            int mid = start + (end-start) / 2;
+
+            if (check(mid)) {
+                start = mid+1;
+            } else end = mid-1;
+        }
+
+        System.out.println(start);
+    }
+
+    private static boolean check(int mid) {
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < c; i++) {
+            String key = "";
+            for (int j = mid + 1; j < r; j++) {
+                key += arr[j][i];
+            }
+            if (set.contains(key)) return false;
+            else set.add(key);
+        }
+
+        return true;
     }
 }
