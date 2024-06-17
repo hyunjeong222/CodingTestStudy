ff1116d81eee99a4bbfea933a5be7bdb9ac6beeb
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-06-17 17:21:37 +0900

    [Gold IV] Title: 도둑, Time: 556 ms, Memory: 102588 KB -BaekjoonHub

--- 백준/Gold/13422. 도둑/도둑.java
+++ 백준/Gold/13422. 도둑/도둑.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int n, m, k;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine()); // 테스트 케이스 수
@@ -11,37 +13,39 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         while (t --> 0) {
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken()); // 집의 개수
-            int m = Integer.parseInt(st.nextToken()); // 연속된 집의 개수
-            int k = Integer.parseInt(st.nextToken()); // 최소 돈의 양
+            n = Integer.parseInt(st.nextToken()); // 집의 개수
+            m = Integer.parseInt(st.nextToken()); // 연속된 집의 개수
+            k = Integer.parseInt(st.nextToken()); // 최소 돈의 양
 
-            int[] arr = new int[n]; // 집에 보관중인 돈의 양
+            arr = new int[n]; // 집에 보관중인 돈의 양
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            int cnt = 0; // 돈을 훔치는 방법의 가짓수
-            // m : 슬라이딩 윈도우의 구간
-            int sum = 0;
-            for (int i = 0; i < m; i++) {
-                sum += arr[i];
-            }
-            
-            if (sum < k) cnt++; // K원 이상의 돈을 훔친다면 자동 방범장치가 작동 -> k원 미만
-
-            if (n == m) {
-                sb.append(cnt).append("\n");
-                continue;
-            }
-
-            for (int i = m; i < arr.length+(m-1); i++) {
-                sum = sum + (arr[i%n]-arr[i-m]);
-
-                if (sum < k) cnt++;
-            }
+            int cnt = getCnt();
             sb.append(cnt).append("\n");
         }
         System.out.println(sb);
+    }
+
+    private static int getCnt() {
+        int cnt = 0; // 돈을 훔치는 방법의 가짓수
+        // m : 슬라이딩 윈도우의 구간
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            sum += arr[i];
+        }
+        if (sum < k) cnt++; // K원 이상의 돈을 훔친다면 자동 방범장치가 작동 -> k원 미만
+
+        if (n == m)  return cnt;
+
+        for (int i = m; i < arr.length+(m-1); i++) {
+            sum = sum + (arr[i%n]-arr[i-m]);
+
+            if (sum < k) cnt++;
+        }
+
+        return cnt;
     }
 }
