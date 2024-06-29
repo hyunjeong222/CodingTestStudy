20a85d5704363b660be01833dd20775f928afe57
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-06-29 23:01:43 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 68 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[31];
+        for (int i = 0; i < 28; i++) {
+            int n = Integer.parseInt(br.readLine());
+            arr[n] = n;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 30; i++) {
+            if (arr[i] == 0) sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
