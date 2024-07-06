ad558e8863ddfa2ca8e19d5a157b1e8f9a10f898
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-07-06 22:01:26 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -8,13 +8,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         int[] arr = new int[26];
-        boolean[] checked = new boolean[26];
         Arrays.fill(arr, -1);
         for (int i = 0; i < str.length(); i++) {
-            if (!checked[str.charAt(i)-97]) {
-                arr[str.charAt(i)-97] = i;
+            int alpha = str.charAt(i)-97;
+            if (arr[alpha] == -1) {
+                arr[alpha] = i;
             }
-            checked[str.charAt(i)-97] = true;
         }
         StringBuilder sb = new StringBuilder();
         for (int i : arr) {
