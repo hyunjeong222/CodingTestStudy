58fb3c413d7bae1ff67cca6bb92715b01cfb7739
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-07-09 18:47:58 +0900

    [Gold IV] Title: 목장 건설하기, Time: 436 ms, Memory: 76692 KB -BaekjoonHub

---
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken()); // 세로
+        int n = Integer.parseInt(st.nextToken()); // 가로
+        int[][] map = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int ans = 0;
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (map[i-1][j-1] == 0) {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
