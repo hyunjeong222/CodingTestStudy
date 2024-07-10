0079a5cf53e2b3a9e6da0ff9bd133aec66940553
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-07-10 13:41:34 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 220 ms, Memory: 26380 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -1,32 +1,32 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n+1][m+1];
-        for (int i = 1 ; i <= n; i++) {
+        int[][] map = new int[n][m];
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for (int j = 1; j <= m; j++) {
-                arr[i][j] = str.charAt(j-1) - '0';
+            for (int j = 0; j < m; j++) {
+                map[i][j] = str.charAt(j)-'0';
             }
         }
-        int ans = Integer.MIN_VALUE;
-        for (int i = 1 ; i <= n; i++) {
+        int l = 0;
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (arr[i][j] == 1) {
-                    arr[i][j] = Math.min(Math.min(arr[i-1][j],arr[i][j-1]),arr[i-1][j-1])+1;
-                    ans = Math.max(ans, arr[i][j]);
+                if (map[i-1][j-1] == 1) {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+                    l = Math.max(l, dp[i][j]);
                 }
             }
         }
-        bw.append(ans*ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        int ans = l * l;
+        System.out.println(ans);
     }
 }
