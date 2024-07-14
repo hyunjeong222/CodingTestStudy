b67baa8d6656b0b770b4aad4be0562031aa175ea
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-07-14 19:04:10 +0900

    [Silver V] Title: 막대기, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < 7; i++) {
+            if ((x & (1 << i)) > 0) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
