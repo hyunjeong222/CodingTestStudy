95e095fdb7a9e5ac49a7f2c69e48bbd7f919af8a
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-07-19 16:22:17 +0900

    [Gold IV] Title: 거짓말, Time: 84 ms, Memory: 11940 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -18,17 +18,19 @@ public class Main {
         }
 
         st = new StringTokenizer(br.readLine());
-        int truthNum = Integer.parseInt(st.nextToken()); // 진실을 아는 사람의 수
-        int[] truthPeople = new int[truthNum];
-        if (truthNum == 0) { // 진실을 아는 사람이 없다면 모든 파티에서 과장된 이야기 가능
+        int truthCnt = Integer.parseInt(st.nextToken()); // 진실을 아는 사람 수
+
+        if (truthCnt == 0) { // 진실을 아는 사람이 없다면 모든 파티 참석 가능
             System.out.println(m);
             System.exit(0);
         }
-        for (int i = 0; i < truthNum; i++) {
+
+        int[] truthPeople = new int[truthCnt+1];
+        for (int i = 1; i <= truthCnt; i++) {
             truthPeople[i] = Integer.parseInt(st.nextToken());
         }
-        // 진실을 아는 사람들 집합 만들기
-        for (int i = 0; i < truthNum-1; i++) {
+        // 진실을 아는 사람 하나의 집합으로 묶어주기
+        for (int i = 1; i < truthCnt; i++) {
             union(truthPeople[i], truthPeople[i+1]);
         }
 
@@ -36,23 +38,26 @@ public class Main {
         for (int i = 0; i <= m; i++) {
             list.add(new ArrayList<>());
         }
+
         for (int i = 1; i <= m; i++) {
             st = new StringTokenizer(br.readLine());
-            int partyNum = Integer.parseInt(st.nextToken());
-            int[] partyPeople = new int[partyNum];
-            for (int j = 0; j < partyNum; j++) {
+            int partyNum = Integer.parseInt(st.nextToken()); // 각 파티에 오는 사람의 수
+            int[] partyPeople = new int[partyNum+1];
+            for (int j = 1; j <= partyNum; j++) {
                 partyPeople[j] = Integer.parseInt(st.nextToken());
                 list.get(i).add(partyPeople[j]);
             }
-            for (int j = 0; j < partyNum-1; j++) {
-                union(partyPeople[j] , partyPeople[j+1]);
+
+            // 파티에 오는 사람들 하나의 집합으로 묶어주기
+            for (int j = 1; j < partyNum; j++) {
+                union(partyPeople[j], partyPeople[j+1]);
             }
         }
 
         int ans = m;
         for (int i = 1; i <= m; i++) {
             for (int j = 0; j < list.get(i).size(); j++) {
-                if (findSameGroup(truthPeople[0], list.get(i).get(j))) {
+                if (findSameGroup(truthPeople[1], list.get(i).get(j))) {
                     ans--;
                     break;
                 }
@@ -62,21 +67,22 @@ public class Main {
         System.out.println(ans);
     }
 
-    private static boolean findSameGroup(int x, int y) {
-        x = find(x);
-        y = find(y);
-        return x == y;
+    private static boolean findSameGroup(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        return a == b;
     }
 
-    private static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
 
-        if (x != y) parent[x] = y;
+        if (a != b) parent[a] = b;
     }
 
-    private static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
+    private static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
     }
 }
