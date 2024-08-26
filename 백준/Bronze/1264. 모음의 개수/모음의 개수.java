c3555fcbfa4790706505505af7ad66faf9866639
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-08-26 14:31:04 +0900

    [Bronze IV] Title: 모음의 개수, Time: 64 ms, Memory: 11312 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String str = br.readLine().toLowerCase();
+            int cnt = 0;
+            if (str.equals("#")) break;
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') cnt++;
+            }
+
+            sb.append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
