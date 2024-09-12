c8f307b7c3d9b2ea9eda0b107ebe0899ef04f6b1
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-09-13 00:27:06 +0900

    [Gold III] Title: 전기가 부족해, Time: 376 ms, Memory: 46036 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.java
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.java
@@ -3,14 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m, k;
-    static ArrayList<Pos> list;
+    static PriorityQueue<Pos> pq;
+    static int[] parent;
     static public class Pos implements Comparable<Pos> {
         int u; int v; int w;
-        public Pos (int u, int v, int w) {
+        public Pos(int u, int v, int w) {
             this.u = u; this.v = v; this.w = w;
         }
         @Override
@@ -18,28 +20,25 @@ public class Main {
             return this.w - o.w;
         }
     }
-    static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); // 도시의 개수
+        n = Integer.parseInt(st.nextToken()); // 도시 개수
         m = Integer.parseInt(st.nextToken()); // 설치 가능한 케이블 수
-        k = Integer.parseInt(st.nextToken()); // 발전소의 개수
+        k = Integer.parseInt(st.nextToken()); // 발전소 개수
 
         parent = new int[n+1];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n ; i++) {
             parent[i] = i;
         }
-
         st = new StringTokenizer(br.readLine());
         int idx;
         for (int i = 0; i < k; i++) {
             idx = Integer.parseInt(st.nextToken());
-            // 발전소 위치는 부모 노드 -1 설정
             parent[idx] = -1;
         }
 
-        list = new ArrayList<>();
+        pq = new PriorityQueue<>();
         int u, v, w;
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -47,22 +46,16 @@ public class Main {
             v = Integer.parseInt(st.nextToken());
             w = Integer.parseInt(st.nextToken());
 
-            list.add(new Pos(u, v, w));
+            pq.offer(new Pos(u, v, w));
         }
 
-        // 간선 데이터 오름차순 정렬
-        Collections.sort(list);
+        int ans = 0;
+        while (!pq.isEmpty()) {
+            Pos now = pq.poll();
 
-        int ans = 0; // 최소비용
-        // 크루스칼 알고리즘 진행
-        for (Pos next : list) {
-            // 부모 노드가 다를때만 (사이클 X)
-            if (find(next.u) != find(next.v)) {
-                union(next.u, next.v);
-                ans += next.w;
-
-                // 모든 도시가 발전소에 의해 전기 공급을 받고있다면 종료
-                if (isAll()) break;
+            if (find(now.u) != find(now.v)) {
+                union(now.u, now.v);
+                ans += now.w;
             }
         }
 
@@ -70,26 +63,21 @@ public class Main {
         br.close();
     }
 
-    private static boolean isAll() {
-        for (int i = 1; i <= n; i++) {
-            if (parent[i] != -1) return false;
-        }
+    private static void union(int u, int v) {
+        u = find(u);
+        v = find(v);
 
-        return true;
-    }
-
-    private static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-
-        if (x != y) {
-            if (x == -1) { // x만 발전소에 연결
-                parent[y] = x;
-            } else if (y == -1) { // y만 발전소에 연결
-                parent[x] = y;
+        if (u != v) {
+            if (u == -1) {
+                parent[v] = -1;
+            } else if (v == -1) {
+                parent[u] = -1;
             } else {
-                if (x > y) parent[x] = y;
-                else parent[y] = x;
+                if (u > v) {
+                    parent[u] = v;
+                } else {
+                    parent[v] = u;
+                }
             }
         }
     }
