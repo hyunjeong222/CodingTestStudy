d042b757491c58a126e0fdb6880ead98292acd19
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-09-18 15:25:49 +0900

    [Gold III] Title: 백양로 브레이크, Time: 312 ms, Memory: 30236 KB -BaekjoonHub

---
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] map;
+    static final int INF = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) continue;
+                map[i][j] = INF;
+            }
+        }
+
+        int u, v, w;
+        while (m --> 0) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+
+            map[u][v] = 0;
+            map[v][u] = (w == 0) ? 1 : 0;
+        }
+
+        for (int k = 1; k <= n; k++) { // 경유지
+            for (int i = 1; i <= n; i++) { // 출발지
+                for (int j = 1; j <= n; j++) { // 도착지
+                    if (i == k) continue;
+                    if (map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int s, e;
+        while (k --> 0) {
+            st = new StringTokenizer(br.readLine());
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            sb.append(map[s][e]).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
