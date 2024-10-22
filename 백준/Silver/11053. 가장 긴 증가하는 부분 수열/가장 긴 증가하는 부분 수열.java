f06b9b51107e12b37a8b682658ae446edf8691b7
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-10-22 15:56:59 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 68 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,28 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int n;
+    static int[] arr, memo;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int[] dp = new int[n+1];
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = 1;
-            for (int j = 1; j < i; j++) {
-                if (arr[i] > arr[j] && dp[j]+1 > dp[i]) {
-                    dp[i] = dp[j]+1;
-                }
+
+        memo = new int[n+1];
+        int len = 0; int idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > memo[len]) {
+                memo[++len] = arr[i];
+            } else {
+                idx = binarySearch(0, len, arr[i]);
+                memo[idx] = arr[i];
             }
-            ans = Math.max(ans, dp[i]);
         }
-        System.out.println(ans);
+
+        System.out.println(len);
+    }
+
+    private static int binarySearch(int left, int right, int target) {
+        while (left < right) {
+            int mid = left + (right-left) / 2;
+            if (memo[mid] < target) {
+                left = mid+1;
+            } else right = mid;
+        }
+
+        return right;
     }
 }
