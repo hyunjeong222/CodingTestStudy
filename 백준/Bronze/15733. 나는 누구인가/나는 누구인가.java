06b917cc200aea3a816017ef2f4befee8ef30937
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-10-27 23:15:56 +0900

    [Bronze V] Title: 나는 누구인가, Time: 64 ms, Memory: 11192 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final int n = 5;
+    public static void main(String[] args) throws IOException {
+        System.out.println("I'm Sexy");
+    }
+}
