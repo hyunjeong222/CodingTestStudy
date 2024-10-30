116685e83de70541cd017458aa0f0d617eb6e8e8
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-10-30 14:43:19 +0900

    [Silver II] Title: 외판원 순회 2, Time: 72 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n;
     static int[][] map;
     static boolean[] checked;
-    static int cnt;
+    static int cnt = 0;
     static int ans = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,17 +20,18 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        // System.out.println(Arrays.deepToString(map));
-
         checked = new boolean[n];
+        // 모든 경로를 방문해야하니 0 먼저 방문한다고 가정
         checked[0] = true;
         cnt += 1;
+
         dfs(0, 0, 0);
 
         System.out.println(ans);
     }
 
-    private static void dfs(int cost, int start, int cur) {
+    private static void dfs(int start, int cur, int cost) {
+        // 마지막 도시에서 처음 도시로 가는 루트가 존재할 때
         if (cnt == n && map[cur][start] != 0) {
             ans = Math.min(ans, cost+map[cur][start]);
             return;
@@ -44,7 +44,7 @@ public class Main {
             checked[i] = true;
             cnt += 1;
 
-            dfs(cost+map[cur][i], start, i);
+            dfs(start, i, cost+map[cur][i]);
 
             cnt -= 1;
             checked[i] = false;
