3e1683ce6043f9b2698224c4c553fa8f528aea2f
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-11-05 20:20:42 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 15412 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -14,7 +14,6 @@ public class Main {
             this.x = x; this.time = time;
         }
     }
-    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,11 +22,16 @@ public class Main {
 
         checked = new boolean[100001];
 
-        bfs(n);
+        if (n >= k) { // 수빈이가 동생보다 앞에 있다면 뒤로 가는 방법만 존재
+            System.out.println(n-k);
+            System.exit(0);
+        }
+
+        int ans = bfs(n);
         System.out.println(ans);
     }
 
-    private static void bfs(int n) {
+    private static int bfs(int n) {
         Queue<Pos> que = new LinkedList<>();
         que.offer(new Pos(n, 0));
         checked[n] = true;
@@ -35,14 +39,13 @@ public class Main {
         int next, time;
         while (!que.isEmpty()) {
             Pos now = que.poll();
-
+            
             if (now.x == k) {
-                ans = now.time;
-                return;
+                return now.time;
             }
 
             for (int i = 0; i < 3; i++) {
-                if (i == 0) {
+                if (i == 0) { // 연산 순서 중요 *
                     next = now.x*2;
                     time = now.time;
                 } else if (i == 1) {
@@ -58,6 +61,8 @@ public class Main {
                 que.offer(new Pos(next, time));
             }
         }
+        
+        return -1;
     }
 
     private static boolean rangeCheck(int next) {
