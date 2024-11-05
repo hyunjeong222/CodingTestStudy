e1f38640f4295667486d6f044897e436b1ea0c2d
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-11-05 18:17:25 +0900

    [Silver I] Title: 숨바꼭질, Time: 112 ms, Memory: 17252 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -8,57 +8,53 @@ import java.util.StringTokenizer;
 public class Main {
     static int n, k;
     static boolean[] checked;
-    static Queue<Pos> que;
     static public class Pos {
         int x; int time;
-        public Pos(int x, int time) {
+        public Pos (int x, int time) {
             this.x = x; this.time = time;
         }
     }
-    static int ans = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken()); // 수빈이 위치
         k = Integer.parseInt(st.nextToken()); // 동생의 위치
-        checked = new boolean[100001];
-        que = new LinkedList<>();
-        que.offer(new Pos(n, 0));
-        bfs();
+
+        int ans = bfs(n);
         System.out.println(ans);
+
+        br.close();
     }
 
-    private static void bfs() {
+    private static int bfs(int n) {
+        Queue<Pos> que = new LinkedList<>();
+        checked = new boolean[100001];
+        que.offer(new Pos(n, 0));
+        checked[n] = true;
+
+        int next;
         while (!que.isEmpty()) {
             Pos now = que.poll();
-            int cur = now.x;
-            int time = now.time;
 
-            if (cur == k) {
-                ans = time;
-                return;
+            if (now.x == k) {
+                return now.time;
             }
 
-            checked[cur] = true;
+            for (int i = 0; i < 3; i++) {
+                if (i == 0) next = now.x-1;
+                else if (i == 1) next = now.x+1;
+                else next = now.x * 2;
 
-            if (check(cur+1) && !checked[cur+1]) {
-                checked[cur+1] = true;
-                que.offer(new Pos(cur+1, time+1));
-            }
-
-            if (check(cur-1) && !checked[cur-1]) {
-                checked[cur-1] = true;
-                que.offer(new Pos(cur-1, time+1));
-            }
-
-            if (check(cur*2) && !checked[cur*2]) {
-                checked[cur*2] = true;
-                que.offer(new Pos(cur*2, time+1));
+                if (rangeCheck(next) || checked[next]) continue;
+                checked[next] = true;
+                que.offer(new Pos(next, now.time+1));
             }
         }
+
+        return -1;
     }
 
-    private static boolean check(int x) {
-        return x >= 0 && x < 100001;
+    private static boolean rangeCheck(int next) {
+        return next < 0 || next >= 100001;
     }
 }
