b6a745e2e7aec080635fe2fb5ad889e5e21def95
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-11-21 20:50:26 +0900

    [Gold III] Title: 보스몬스터 전리품, Time: 220 ms, Memory: 25792 KB -BaekjoonHub

--- 백준/Gold/20005. 보스몬스터 전리품/보스몬스터 전리품.java
+++ 백준/Gold/20005. 보스몬스터 전리품/보스몬스터 전리품.java
@@ -5,90 +5,100 @@ import java.util.*;
 
 public class Main {
     static int m, n, p;
+    static int bossX, bossY, hp;
     static char[][] map;
+    static int[] attack;
     static Queue<Pos> que;
-    static HashMap<Character, Player> playerMap;
     static public class Pos {
-        char player;
-        int x; int y;
-        public Pos(char player, int x, int y) {
-            this.player = player;
-            this.x = x; this.y = y;
-        }
-    }
-    static public class Player {
-        int id; int dps;
-        public Player(int id, int dps) {
-            this.id = id; this.dps = dps;
+        int x; int y; int time;
+        public Pos(int x, int y, int time) {
+            this.x = x; this.y = y; this.time = time;
         }
     }
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        p = Integer.parseInt(st.nextToken()); // 플레이어 수
-        map = new char[m][n];
-        que = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < n; j++) {
-                map[i][j] = str.charAt(j);
-                if (map[i][j] != '.' && map[i][j] != 'X' && map[i][j] != 'B') { // 플레이어 아이디
-                    que.offer(new Pos(map[i][j], i, j)); // 플레이어와 현재 위치 저장
-                }
-            }
-        }
-
-        playerMap = new HashMap<>();
-        int num = 0; // 플레이어 고유번호, 각 플레이어의 방문체크를 위해
-        for (int i = 0; i < p; i++) {
-            st = new StringTokenizer(br.readLine());
-            char id = st.nextToken().charAt(0);
-            int dps = Integer.parseInt(st.nextToken()); // 1초당 보스몬스터의 체력을 줄일 수 있는지 의미
-            playerMap.put(id, new Player(num++, dps));
-        }
-
-        int hp = Integer.parseInt(br.readLine()); // 보스 모스터 체력
-
-        int ans = bfs(hp);
+        init();
+        
+        int ans = bfs();
         System.out.println(ans);
     }
 
-    private static int bfs(int hp) {
-        boolean[][][] checked = new boolean[m][n][p];
-        ArrayList<Character> attack = new ArrayList<>(); // 보스랑 같은 위치라 공격할 수 있는 플레이어
+    private static int bfs() {
+        que = new LinkedList<>();
+        boolean[][] checked = new boolean[m][n];
 
-        while (hp > 0) {
-            int size = que.size();
-            while (size-- > 0) {
-                Pos now = que.poll();
-                
-                for (int i = 0; i < 4; i++) {
-                    int nx = dx[i] + now.x;
-                    int ny = dy[i] + now.y;
+        que.offer(new Pos(bossX, bossY, 0));
+        checked[bossX][bossY] = true;
 
-                    if (rangeCheck(nx, ny)) continue;
-                    // 현재 플레이어가 방문했던 곳이나 길이 X
-                    if (checked[nx][ny][playerMap.get(now.player).id] || map[nx][ny] == 'X') continue;
+        int totalPower = 0; // 보스를 공격할 수 있는 플레이어들의 총 파워
+        int playerCnt = 0; // 전리품을 가져갈 수 있는 플레이어의 수
+        int prev = 0; // 이전시간
 
-                    checked[nx][ny][playerMap.get(now.player).id] = true;
-                    if (map[nx][ny] == 'B') attack.add(now.player);
-                    else que.offer(new Pos(now.player, nx, ny));
-                }
+        while (!que.isEmpty()) {
+            Pos now = que.poll();
+
+            if (now.time != prev) {
+                hp -= totalPower;
+                prev = now.time;
+
+                if (hp <= 0) break; // 보스를 잡았다면 종료
             }
 
-            for (char p : attack) { // 공격할 수 있는 모든 플레이어 공격
-                hp -= playerMap.get(p).dps;
+            if (map[now.x][now.y] != '.') { // 보스가 플레이어를 만났다면
+                int player = map[now.x][now.y]-'a';
+                playerCnt++; // 플레이어 증가
+                totalPower += attack[player]; // 만난 플레이어의 공격력 누적
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + now.x;
+                int ny = dy[i] + now.y;
+
+                if (rangeCheck(nx, ny)) continue;
+                if (checked[nx][ny] || map[nx][ny] == 'X') continue;
+
+                checked[nx][ny] = true;
+                que.offer(new Pos(nx, ny, now.time+1));
             }
         }
 
-        return attack.size();
+        return playerCnt;
     }
 
     private static boolean rangeCheck(int x, int y) {
         return x < 0 || x >= m || y < 0 || y >= n;
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken()); // 플레이어수
+
+        map = new char[m][n];
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = str.charAt(j);
+                if (map[i][j] == 'B') {
+                    bossX = i; bossY = j;
+                    map[i][j] = '.';
+                }
+            }
+        }
+
+        attack = new int[p];
+        for (int i = 0; i < p; i++) {
+            st = new StringTokenizer(br.readLine());
+            int player = st.nextToken().charAt(0)-'a';
+            int dps = Integer.parseInt(st.nextToken()); // 공격력
+            attack[player] = dps;
+        }
+
+        hp = Integer.parseInt(br.readLine()); // 보스몬스터의 체력
+
+        br.close();
     }
 }
