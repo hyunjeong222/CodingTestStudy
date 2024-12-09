821d109157d66b2fd8ec8078251f347a2fb36350
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-12-09 18:51:25 +0900

    [Bronze IV] Title: Identifying tea, Time: 68 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Bronze/11549. Identifying tea/Identifying tea.java
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -1,24 +1,23 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static final int N = 5;
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[] arr = new int[5];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int correct = Integer.parseInt(br.readLine());
+        int[] ansArr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            ansArr[i] = Integer.parseInt(st.nextToken());
+        }
+        int ans = 0;
+        for (int i : ansArr) {
+            if (correct == i) ans++;
+        }
 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-
-		int cnt = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == T) {
-				cnt++;
-			}
-		}
-        
-		System.out.println(cnt);
-		sc.close();
+        System.out.println(ans);
     }
 }
