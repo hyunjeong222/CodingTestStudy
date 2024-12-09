0e32be346eb9130010c8c2a6b73d4a99ef046de0
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2024-12-09 19:35:46 +0900

    [Silver III] Title: 크리스마스 선물, Time: 296 ms, Memory: 34876 KB -BaekjoonHub

---
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        int a;
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            if (pq.isEmpty() && a == 0) {
+                sb.append(-1).append("\n");
+            } else if (!pq.isEmpty() && a == 0) {
+                sb.append(pq.poll()).append("\n");
+            } else {
+                for (int j = 0; j < a; j++) {
+                    pq.offer(Integer.parseInt(st.nextToken()));
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
