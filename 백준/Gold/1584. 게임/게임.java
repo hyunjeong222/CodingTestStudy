c55a86886caefdf5f633af5f8691be7fbb644b6d
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-01-04 12:42:06 +0900

    [Gold V] Title: 게임, Time: 300 ms, Memory: 24132 KB -BaekjoonHub

--- 백준/Gold/1584. 게임/게임.java
+++ 백준/Gold/1584. 게임/게임.java
@@ -1,31 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+    static final int INF = 987654321;
     static final int WARNING = 1;
     static final int DEATH = 2;
-    static final int INF = 987654321;
     static int[][] map;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static PriorityQueue<Pos> pq;
     static public class Pos implements Comparable<Pos> {
-        int x; int y; int dist;
+        int x; int y;
+        int dist;
         public Pos (int x, int y, int dist) {
-            this.x = x; this.y = y; this.dist = dist;
+            this.x = x; this.y = y;
+            this.dist = dist;
         }
         @Override
-        public int compareTo (Pos o) {
+        public int compareTo(Pos o) {
             return this.dist - o.dist;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         map = new int[501][501];
-
-        int n = Integer.parseInt(br.readLine()); // 구역의 수
+        int n = Integer.parseInt(br.readLine()); // 위험한 구역의 수
+        StringTokenizer st;
+        // 위험한 구역의 정보
         int x1, y1, x2, y2;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -37,7 +42,8 @@ public class Main {
             marking(x1, y1, x2, y2, WARNING);
         }
 
-        int m = Integer.parseInt(br.readLine()); // 구역의 수
+        int m = Integer.parseInt(br.readLine()); // 죽음의 구역의 수
+        // 죽음의 구역의 정보
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             x1 = Integer.parseInt(st.nextToken());
@@ -48,40 +54,34 @@ public class Main {
             marking(x1, y1, x2, y2, DEATH);
         }
 
-        int ans = bfs();
+        int ans = dijkstra();
 
         System.out.println(ans);
     }
 
-    private static void marking(int x1, int y1, int x2, int y2, int mark) {
-        for (int i = Math.min(x1, x2); i <= Math.max(x1, x2); i++) {
-            for (int j = Math.min(y1, y2); j <= Math.max(y1, y2); j++) {
-                map[i][j] = mark;
-            }
-        }
-    }
-
-    private static int bfs() {
-        PriorityQueue<Pos> dq = new PriorityQueue<>();
-        dq.offer(new Pos(0, 0, 0));
+    private static int dijkstra() {
         int[][] dist = new int[501][501];
         for (int[] row : dist) Arrays.fill(row, INF);
+
+        pq = new PriorityQueue<>();
+        pq.offer(new Pos(0, 0, 0));
         dist[0][0] = 0;
 
-        while (!dq.isEmpty()) {
-            Pos cur = dq.poll();
-            int curDist = cur.dist;
+        while (!pq.isEmpty()) {
+            Pos now = pq.poll();
+            int nowDist = now.dist;
 
             for (int d = 0; d < 4; d++) {
-                int nr = cur.x + dx[d];
-                int nc = cur.y + dy[d];
+                int nx = dx[d] + now.x;
+                int ny = dy[d] + now.y;
 
-                if (rangeCheck(nr, nc) || map[nr][nc] == DEATH) continue;
-                if (dist[nr][nc] <= curDist + map[nr][nc]) continue;
+                if (rangeCheck(nx, ny) || map[nx][ny] == DEATH) continue;
 
-                dist[nr][nc] = curDist + map[nr][nc];
-                if (map[nr][nc] == 0) dq.offer(new Pos(nr, nc, curDist));
-                else dq.offer(new Pos(nr, nc, curDist+1));
+                if (dist[nx][ny] <= nowDist + map[nx][ny]) continue;
+
+                dist[nx][ny] = nowDist + map[nx][ny];
+                if (map[nx][ny] == 0) pq.offer(new Pos(nx, ny, nowDist));
+                else pq.offer(new Pos(nx, ny, nowDist+1));
             }
         }
 
@@ -90,5 +90,13 @@ public class Main {
 
     private static boolean rangeCheck(int x, int y) {
         return x < 0 || x > 500 || y < 0 || y > 500;
+    }
+
+    private static void marking(int x1, int y1, int x2, int y2, int mark) {
+        for (int i = Math.min(x1, x2); i <= Math.max(x1, x2); i++) {
+            for (int j = Math.min(y1, y2); j <= Math.max(y1, y2); j++) {
+                map[i][j] = mark;
+            }
+        }
     }
 }
