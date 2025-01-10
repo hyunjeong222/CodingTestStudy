44207745c3b2d565428e4a77f2fc9a178e9b4209
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-01-11 01:37:43 +0900

    [Gold III] Title: 소가 길을 건너간 이유 6, Time: 300 ms, Memory: 32584 KB -BaekjoonHub

--- 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.java
+++ 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.java
@@ -24,18 +24,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken()); // n*n
-        k = Integer.parseInt(st.nextToken()); // 소 개수
-        r = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken()); // 소의 수
+        r = Integer.parseInt(st.nextToken()); // 길의 위치 정보
 
-        cow = new int[k][2];
-
-        road = new ArrayList[n][n];
+        road = new ArrayList[n][n]; // 길에 대한 정보 저장
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 road[i][j] = new ArrayList<>();
             }
         }
-
         int a, b, c, d;
         for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
@@ -47,51 +44,57 @@ public class Main {
             road[a][b].add(new Pos(c, d));
             road[c][d].add(new Pos(a, b));
         }
-
+        
+        cow = new int[k][2]; // 소가 있는 위치
         for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
             a = Integer.parseInt(st.nextToken())-1;
             b = Integer.parseInt(st.nextToken())-1;
-
+            
             cow[i][0] = a;
             cow[i][1] = b;
         }
 
+        // 결과적으로 쌍을 구하는 것이기 때문에 자기보다 뒤에 있는 소들만 계산하면 된다.
         for (int i = 0; i < k; i++) {
             bfs(cow[i][0], cow[i][1], i);
         }
 
+        // 길을 건너지 않으면 만날 수 없는 소가 몇 쌍 ?
         System.out.println(ans);
     }
 
     private static void bfs(int x, int y, int idx) {
         Queue<Pos> que = new LinkedList<>();
         que.offer(new Pos(x, y));
+
         checked = new boolean[n][n];
         checked[x][y] = true;
 
         while (!que.isEmpty()) {
             Pos now = que.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + now.x;
-                int ny = dy[i] + now.y;
+            for (int d = 0; d < 4; d++) {
+                int nx = dx[d] + now.x;
+                int ny = dy[d] + now.y;
 
                 if (rangeCheck(nx, ny)) continue;
 
-                boolean flag = false;
-                for (int j = 0; j < road[now.x][now.y].size(); j++) {
-                    Pos r = road[now.x][now.y].get(j);
-                    if (nx == r.x && ny == r.y) {
+                boolean flag = false; // 길이 있는지 체크
+                for (int i = 0; i < road[now.x][now.y].size(); i++) {
+                    Pos tmp = road[now.x][now.y].get(i); // 길 정보
+
+                    if (nx == tmp.x && ny == tmp.y) {
                         flag = true;
                         break;
                     }
                 }
 
+                // 길이 있다면 탐색 X
                 if (flag || checked[nx][ny]) continue;
 
-                que.offer(new Pos(nx, ny));
                 checked[nx][ny] = true;
+                que.offer(new Pos(nx, ny));
             }
         }
 
