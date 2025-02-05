de8c7784bdd0ff1f4259ad63df1040686cfc125b
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-02-05 15:01:20 +0900

    [Gold V] Title: Charm Bracelet, Time: 172 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Gold/6144. Charm Bracelet/Charm Bracelet.java
+++ 백준/Gold/6144. Charm Bracelet/Charm Bracelet.java
@@ -8,25 +8,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()); // 매력의 개수
-        int m = Integer.parseInt(st.nextToken()); // 최대 용량
+        int m = Integer.parseInt(st.nextToken()); // 무게 제한
 
-        int[] w = new int[n+1]; // 무게
-        int[] v = new int[n+1]; // 가치
-        for (int i = 1; i <= n; i++) {
+        int[] dp = new int[m+1];
+        int w, v;
+        while (n --> 0) {
             st = new StringTokenizer(br.readLine());
-            w[i] = Integer.parseInt(st.nextToken());
-            v[i] = Integer.parseInt(st.nextToken());
-        }
+            w = Integer.parseInt(st.nextToken()); // 이번 매력 무게
+            v = Integer.parseInt(st.nextToken()); // 이번 매력 가치
 
-        int[][] dp = new int[n+1][m+1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (w[i] <= j) {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]);
-                } else dp[i][j] = dp[i-1][j];
+            for (int i = m; w <= i; --i) {
+                dp[i] = Math.max(dp[i-w]+v, dp[i]);
             }
         }
 
-        System.out.println(dp[n][m]);
+        System.out.println(dp[m]);
     }
 }
