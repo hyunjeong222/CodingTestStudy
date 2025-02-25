b797d36909bdd2b563eaf4bafd177c0fc64842d0
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-02-25 18:40:33 +0900

    [Gold II] Title: 책정리, Time: 268 ms, Memory: 37560 KB -BaekjoonHub

--- 백준/Gold/1818. 책정리/책정리.java
+++ 백준/Gold/1818. 책정리/책정리.java
@@ -1,44 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] books;
-    static int[] memo;
+    static int n;
+    static int[] books, lis;
+    static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         books = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             books[i] = Integer.parseInt(st.nextToken());
         }
 
-        memo = new int[n+1];
+        int lisCnt = getLis();
+
+        System.out.println(n-lisCnt);
+    }
+
+    public static int getLis() {
+        lis = new int[n+1];
         int len = 0, idx;
+
+        Arrays.fill(lis, INF);
+        lis[0] = 0;
+
         for (int i = 0; i < n; i++) {
-            if (books[i] > memo[len]) {
-                len += 1;
-                memo[len] = books[i];
-            } else {
-                idx = binarySearch(0, len, books[i]);
-                memo[idx] = books[i];
-            }
+            idx = binarySearch(0, len+1, books[i]);
+            lis[idx] = books[i];
+            len = Math.max(len, idx);
         }
 
-        System.out.println(n-len);
+        return len;
     }
 
     private static int binarySearch(int start, int end, int target) {
         int mid;
         while (start < end) {
             mid = start + (end-start) / 2;
-
-            if (memo[mid] < target) start = mid+1;
+            if (lis[mid] < target) start = mid+1;
             else end = mid;
         }
-
+        
         return end;
     }
 }
