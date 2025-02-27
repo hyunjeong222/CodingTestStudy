82a370fd51aec048c9dbfbaa07e8128a35d99d1f
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-02-27 17:14:08 +0900

    [Gold I] Title: 3초 정렬, Time: 296 ms, Memory: 41912 KB -BaekjoonHub

--- 백준/Gold/22963. 3초 정렬/3초 정렬.java
+++ 백준/Gold/22963. 3초 정렬/3초 정렬.java
@@ -6,68 +6,70 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n;
-    static int[] arr, length;
-    static final int MAX = 1_000_000_000;
+    static int[] A, length;
+    static final int MAX = 1000000000;
     static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine()); // 수열의 길이
-        arr = new int[n+1]; // 수열
-        length = new int[n]; // 인덱스별 오름차순 수열 길이
+        A = new int[n+1]; // 수열
+        length = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        arr[n] = MAX;
+        A[n] = MAX;
 
-        int lisCnt = getLis(); // LIS 길이
+        // 연산 횟수 구하기
+        int lisCnt = getLis();
         int k = n - lisCnt; // 연산 횟수
 
-        if (k > 3) { // 3번의 연산으로 오름차순을 만들 수 없음
+        // 3번의 연산으로 오름차순을 만들 수 없음
+        if (k > 3) {
             System.out.println("NO");
             return;
         }
 
-        // 오름차순을 만들 수 있음
+        // 3번의 연산으로 오름차순을 만들 수 있음
         StringBuilder sb = new StringBuilder();
         sb.append("YES").append("\n").append(k).append("\n");
 
         int cnt = lisCnt;
         int[] lis = new int[n];
-
         for (int i = n-1; i >= 0; i--) {
             if (length[i] == cnt) {
-                lis[cnt] = arr[i];
+                lis[cnt] = A[i];
                 cnt--;
             }
         }
 
         cnt = lisCnt;
         for (int i = n-1; i >= 0; i--) {
-            if (arr[i] == lis[cnt]) {
+            if (A[i] == lis[cnt]) {
                 cnt--;
                 continue;
             }
 
-            int x = arr[i+1];
+            int x = A[i+1];
             sb.append(i+1).append(" ").append(x).append("\n");
-            arr[i] = x;
+            A[i] = x;
         }
 
-        System.out.println(sb.toString());
+        // System.out.println(Arrays.toString(lis));
 
+        System.out.println(sb.toString());
     }
 
     private static int getLis() {
-        int[] lis = new int[n+1];
         int len = 0, idx;
+        int[] lis = new int[n+1];
 
         Arrays.fill(lis, INF);
         lis[0] = 0;
-        
+
         for (int i = 0; i < n; i++) {
-            idx = binarySearch(0, len+1, arr[i], lis);
-            lis[idx] = arr[i];
+            idx = binarySearch(0, len+1, A[i], lis);
+            lis[idx] = A[i];
             length[i] = idx;
             len = Math.max(len, idx);
         }
@@ -78,11 +80,10 @@ public class Main {
     private static int binarySearch(int start, int end, int target, int[] lis) {
         int mid;
         while (start < end) {
-            mid = start + (end - start) / 2;
-            if (lis[mid] <= target)  start = mid+1;
+            mid = start + (end-start) / 2;
+            if (lis[mid] <= target) start = mid+1;
             else end = mid;
         }
-
         return end;
     }
 }
