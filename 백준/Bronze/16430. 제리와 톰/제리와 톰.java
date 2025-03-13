9138d4f89214cf4f7605d489c5b68092b7c1d99f
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-03-13 23:43:03 +0900

    [Bronze V] Title: 제리와 톰, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(B-A).append(" ").append(B);
+
+        System.out.println(sb.toString());
+    }
+}
