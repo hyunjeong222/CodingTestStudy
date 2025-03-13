4c2e08fd4d21be555362fc4c52ba21d51adc9e9d
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-03-14 00:14:48 +0900

    [Bronze V] Title: 세금, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/20492. 세금/세금.java
+++ 백준/Bronze/20492. 세금/세금.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int n = scanner.nextInt();
-        
-        System.out.println((int)(n * 0.78) + " " + (int)(n - (n * 0.044)));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append((int)(n*0.78)).append(" ")
+                .append((int)(n*0.8+n*0.2*0.78)).append("\n");
+
+        System.out.println(sb.toString());
     }
 }
