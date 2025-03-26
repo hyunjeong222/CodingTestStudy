2a16f194c42728efa6e92d5fe5f388c39abd4ac9
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-03-26 18:38:42 +0900

    [Gold II] Title: 후위 표기식, Time: 68 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char now = str.charAt(i);
+
+            switch (now) {
+                case '+' :
+                case '-' :
+                case '*' :
+                case '/' :
+                    while (!stack.isEmpty() && priority(stack.peek()) >= priority(now)) {
+                        sb.append(stack.pop());
+                    }
+                    stack.add(now);
+                    break;
+                case '(' :
+                    stack.add(now);
+                    break;
+                case ')' :
+                    while (!stack.isEmpty() && stack.peek() != '(') {
+                        sb.append(stack.pop());
+                    }
+                    stack.pop();
+                    break;
+                default : sb.append(now);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static int priority(char operator) {
+        if (operator == '(' || operator == ')') {
+            return 0;
+        } else if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+
+        return -1;
+    }
+}
