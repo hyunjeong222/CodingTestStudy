d8db5bf6b9aeab1848ff21d89957ac4ca5218ead
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-03-28 15:38:47 +0900

    [Silver I] Title: 트리 순회, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,46 +1,69 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static Node head = new Node('A', null, null); // 트리는 A부터 시작
-    public static void main(String[] args) throws Exception {
+    static public class Node {
+        char value;
+        Node left; Node right;
+        public Node(char value, Node left, Node right) {
+            this.value = value;
+            this.left = left; this.right = right;
+        }
+    }
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            char root = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
+        char root, left, right;
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            root = st.nextToken().charAt(0);
+            left = st.nextToken().charAt(0);
+            right = st.nextToken().charAt(0);
 
             insertNode(head, root, left, right);
         }
 
         preOrder(head);
-        System.out.println();
+        sb.append("\n");
         inOrder(head);
-        System.out.println();
+        sb.append("\n");
         postOrder(head);
-        System.out.println();
+
+        System.out.println(sb.toString());
     }
 
+    private static void preOrder(Node node) {
+        if (node == null) return;
 
-    static class Node {
-        char value;
-        Node left;
-        Node right;
-
-        Node(char value, Node left, Node right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
+        sb.append(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 
-    public static void insertNode(Node tmp, char root, char left, char right) {
+    private static void inOrder(Node node) {
+        if (node == null) return;
+
+        inOrder(node.left);
+        sb.append(node.value);
+        inOrder(node.right);
+    }
+
+    private static void postOrder(Node node) {
+        if (node == null) return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        sb.append(node.value);
+    }
+
+    private static void insertNode(Node tmp, char root, char left, char right) {
         if (tmp.value == root) {
             tmp.left = (left == '.' ? null : new Node(left, null, null));
             tmp.right = (right == '.' ? null : new Node(right, null, null));
@@ -52,32 +75,5 @@ public class Main {
                 insertNode(tmp.right, root, left, right);
             }
         }
-    }
-
-    private static void preOrder(Node node) {
-        if (node == null) { // 더 이상 값이 없을 때 까지
-            return;
-        }
-        System.out.print(node.value); // 출력
-        preOrder(node.left); // 왼쪽 노드 탐색
-        preOrder(node.right); // 오른쪽 노드 탐색
-    }
-
-    private static void inOrder(Node node) {
-        if (node == null) { // 더 이상 값이 없을 때 까지
-            return;
-        }
-        inOrder(node.left); // 왼쪽 노드 탐색
-        System.out.print(node.value); // 출력
-        inOrder(node.right); // 오른쪽 노드 탐색
-    }
-
-    private static void postOrder(Node node) {
-        if (node == null) { // 더 이상 값이 없을 때 까지
-            return;
-        }
-        postOrder(node.left); // 왼쪽 노드 탐색
-        postOrder(node.right); // 오른쪽 노드 탐색
-        System.out.print(node.value); // 출력
     }
 }
