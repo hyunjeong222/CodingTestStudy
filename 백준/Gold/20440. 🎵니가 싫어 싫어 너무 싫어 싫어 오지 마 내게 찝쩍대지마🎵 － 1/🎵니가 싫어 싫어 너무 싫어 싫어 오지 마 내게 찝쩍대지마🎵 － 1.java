b57ea974a0c1a3f1917c67c56ad2607012b10b02
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-04-14 18:40:41 +0900

    [Gold III] Title: 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1, Time: 744 ms, Memory: 320368 KB -BaekjoonHub

--- 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
+++ 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
@@ -1,17 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static public class Mosquito implements Comparable<Mosquito> {
+        int start; int end;
+        public Mosquito(int start, int end) {
+            this.start = start; this.end = end;
+        }
+        @Override
+        public int compareTo(Mosquito m) {
+            return this.start - m.start;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine()); // 모기의 마릿수
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Mosquito> list = new ArrayList<>();
 
         StringTokenizer st;
         // 모기의 입장 시각, 퇴장 시각
@@ -21,35 +28,35 @@ public class Main {
             sTime = Integer.parseInt(st.nextToken());
             eTime = Integer.parseInt(st.nextToken());
 
-            map.put(sTime, map.getOrDefault(sTime, 0)+1);
-            map.put(eTime, map.getOrDefault(eTime, 0)-1);
+            list.add(new Mosquito(sTime, eTime));
         }
 
         // 모기가 있는 시각별 모기의 수를 누적해서 합해야 함
-        List<Integer> keyList = new ArrayList<>(map.keySet());
-        keyList.sort((o1, o2) -> o1 - o2);
+        Collections.sort(list);
 
-        int sAns = 0, eAns = 0; // 모기가 가장 많이 있는 시간대
-        int max = 0; // 해당 시간대에 최대 모기 마릿수
-        int sum = 0;
-        boolean flag = false; // 최대 구간이 열렸는지
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int start = 0, end = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            int prev = -1;
 
-        for (int key : keyList) {
-            sum += map.get(key);
+            while (!pq.isEmpty() && list.get(i).start >= pq.peek()) {
+                prev = pq.poll();
+            }
 
-            if (sum > max) { // 최댓값 나오면 갱신
-                max = sum;
-                sAns = key;
-                flag = true;
-            } else if (sum < max && flag) { // 최대 구간에서 값이 바뀌는 지점이면
-                eAns = key;
-                flag = false; // 구간 닫음
+            pq.offer(list.get(i).end);
+
+            if (pq.size() > ans) {
+                start = list.get(i).start;
+                end = pq.peek();
+                ans = pq.size();
+            } else if (pq.size() == ans && prev == list.get(i).start) {
+                end = pq.peek();
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        sb.append(max).append("\n");
-        sb.append(sAns).append(" ").append(eAns).append("\n");
+        sb.append(ans).append("\n");
+        sb.append(start).append(" ").append(end).append("\n");
 
         System.out.println(sb.toString());
     }
