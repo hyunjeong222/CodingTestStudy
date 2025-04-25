d527056da188b6757552d1b4eae1703d035353cb
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-04-25 22:03:36 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 516 ms, Memory: 114940 KB -BaekjoonHub

---
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[n][m];
+        int[][][] dp = new int[n][m][3]; // d 방향으로 온 (i, j) 위치
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                Arrays.fill(dp[i][j], INF);
+            }
+        }
+
+        // 초기 설정
+        for (int j = 0; j < m; j++) {
+            dp[0][j][0] = map[0][j];
+            dp[0][j][1] = map[0][j];
+            dp[0][j][2] = map[0][j];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j == 0) {
+                    dp[i][j][0] = Math.min(dp[i-1][j+1][1], dp[i-1][j+1][2]) + map[i][j];
+                    dp[i][j][1] = dp[i-1][j][0] + map[i][j];
+                } else if (j == m-1) {
+                    dp[i][j][2] = Math.min(dp[i-1][j-1][1], dp[i-1][j-1][0]) + map[i][j];
+                    dp[i][j][1] = dp[i-1][j][2] + map[i][j];
+                } else {
+                    dp[i][j][2] = Math.min(dp[i-1][j-1][0], dp[i-1][j-1][1]) + map[i][j];
+                    dp[i][j][1] = Math.min(dp[i-1][j][2], dp[i-1][j][0]) + map[i][j];
+                    dp[i][j][0] = Math.min(dp[i-1][j+1][1], dp[i-1][j+1][2]) + map[i][j];
+                }
+            }
+        }
+
+        int min = INF;
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < 3; i++) {
+                min = Math.min(min, dp[n-1][j][i]);
+            }
+        }
+
+        System.out.println(min);
+
+        br.close();
+    }
+}
