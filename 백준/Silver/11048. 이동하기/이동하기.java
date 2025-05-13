8bb01195d82812e7a45dbc10db36de2a08ad46d5
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-05-14 00:14:56 +0900

    [Silver II] Title: 이동하기, Time: 388 ms, Memory: 79896 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -19,8 +19,9 @@ public class Main {
         int[][] dp = new int[n+1][m+1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                dp[i][j] = Math.max(map[i][j]+dp[i-1][j],
-                        Math.max(map[i][j]+dp[i][j-1], map[i][j]+dp[i-1][j-1]));
+                // 여기서 대각선으로 가는 경로는 오른쪽->아래와 아래->오른쪽을 거쳐가는 것보다
+                // 항상 작을 수밖에 없다.
+                dp[i][j] = Math.max(map[i][j]+dp[i-1][j],map[i][j]+dp[i][j-1]);
             }
         }
 
