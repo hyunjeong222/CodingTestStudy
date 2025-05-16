82ffab6a3573ba26078fe30d59c00fbf2b15847d
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-05-16 16:29:24 +0900

    [Silver III] Title: N과 M (6), Time: 64 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,47 +1,49 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[] arr;
-    static int[] combi;
+    static int[] arr, len;
     static boolean[] checked;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken()); // 자연수
+        m = Integer.parseInt(st.nextToken()); // 길이
 
         arr = new int[n];
-        combi = new int[m];
-        checked = new boolean[n];
-
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
+
+        len = new int[m]; // 길이만큼 수열 만들기
+        checked = new boolean[n];
+
         dfs(0, 0);
-        System.out.println(sb);
+
+        System.out.println(sb.toString());
+
+        br.close();
     }
 
-    private static void dfs(int num, int depth) {
+    private static void dfs(int idx, int depth) {
         if (depth == m) {
-            for (int val : combi) {
-                sb.append(val + " ");
+            for (int i : len) {
+                sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = num; i < n; i++) {
+        for (int i = idx; i < n; i++) {
             if (!checked[i]) {
                 checked[i] = true;
-                combi[depth] = arr[i];
+                len[depth] = arr[i];
                 dfs(i+1, depth+1);
                 checked[i] = false;
             }
