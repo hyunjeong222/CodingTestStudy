c1eb6c4fd8f4a862085bfe8d36927e38c829d60e
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-05-16 15:17:52 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 20464 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,47 +1,44 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static boolean[] checked;
     static int n, m;
+    static int[] len;
+    static boolean[] checked;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken()); // 자연수
+        m = Integer.parseInt(st.nextToken()); // 길이
 
-        arr = new int[m];
+        len = new int[m]; // 길이만큼 수열 만들기
         checked = new boolean[n];
 
         dfs(0);
-        bw.append(sb);
-        bw.flush();
-        bw.close();
+
+        System.out.println(sb.toString());
+
         br.close();
     }
 
-    private static void dfs(int depth) throws IOException {
-        // 재귀의 깊이가 m과 같아지면 탐색과정에서 담았던 배열을 출력
+    private static void dfs(int depth) {
         if (depth == m) {
-            for (int val : arr) {
-                sb.append(val + " ");
+            for (int i : len) {
+                sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            // 만약 해당 노드를 방문하지 않았다면
             if (!checked[i]) {
-                checked[i] = true; // 해당 노드 방문 상태로 변경
-                arr[depth] = i+1; // 해당 깊이를 index로 하여 i+1 값 저장
-                dfs(depth+1); // 다음 자식 노드 방문을 위해 depth 1 증가시키면서 재귀 호출
-
-                checked[i] = false; // 자식 노드 방문이 끝나고 돌아오면 방문 노드를 방문하지 않은 상태로 변경
+                checked[i] = true;
+                len[depth] = i+1;
+                dfs(depth+1);
+                checked[i] = false;
             }
         }
     }
