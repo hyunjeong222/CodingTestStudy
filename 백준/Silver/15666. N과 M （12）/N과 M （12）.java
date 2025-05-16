c4350584d983c4d3c3bf98e4e4b4755ed887fdb7
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-05-16 17:05:49 +0900

    [Silver II] Title: N과 M (12), Time: 80 ms, Memory: 12464 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -1,49 +1,47 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[] arr;
-    static int[] combi;
-    static boolean[] checked;
+    static int[] arr, len;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken()); // 자연수
+        m = Integer.parseInt(st.nextToken()); // 길이
 
         arr = new int[n];
-        combi = new int[m];
-        checked = new boolean[n];
-
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
-        dfs(0,0);
-        System.out.println(sb);
+
+        len = new int[m]; // 길이만큼 수열 만들기
+
+        dfs(0, 0);
+
+        System.out.println(sb.toString());
+
+        br.close();
     }
 
-    private static void dfs(int num, int depth) {
+    private static void dfs(int idx, int depth) {
         if (depth == m) {
-            for (int val : combi) {
-                sb.append(val + " ");
+            for (int i : len) {
+                sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
 
         int before = 0;
-        for (int i = num; i < n; i++) {
-            if (checked[i]) {
-                continue;
-            } else if (before != arr[i]) {
-                combi[depth] = arr[i];
+        for (int i = idx; i < n; i++) {
+            if (arr[i] != before) {
+                len[depth] = arr[i];
                 before = arr[i];
                 dfs(i, depth+1);
             }
