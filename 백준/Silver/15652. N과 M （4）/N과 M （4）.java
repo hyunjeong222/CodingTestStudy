52921cc0992642cffe1371b3c4ee54deb3b8ae6c
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-05-16 15:49:15 +0900

    [Silver III] Title: N과 M (4), Time: 84 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,36 +1,38 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int[] arr;
     static int n, m;
+    static int[] len;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken()); // 자연수
+        m = Integer.parseInt(st.nextToken()); // 길이
 
-        arr = new int[m];
+        len = new int[m]; // 길이만큼 수열 만들기
 
-        dfs(1, 0);
+        dfs( 1, 0);
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
+
         br.close();
     }
 
     private static void dfs(int num, int depth) {
         if (depth == m) {
-            for (int val : arr) {
-                sb.append(val + " ");
+            for (int i : len) {
+                sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
 
         for (int i = num; i <= n; i++) {
-            arr[depth] = i;
+            len[depth] = i;
             dfs(i, depth+1);
         }
     }
