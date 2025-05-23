f313f7ed4dedc7f8e380aa1eb6bd16f1afcc2f45
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-05-24 00:05:47 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 432 ms, Memory: 125352 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,41 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int n;
-    static int[] arr, memo;
+    static int[] A, memo;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        n = Integer.parseInt(br.readLine()); // 수열 A의 크기
+        A = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
         memo = new int[n+1];
-        int len = 0; int idx = 0;
+        int len = 0, idx = 0;
         for (int i = 0; i < n; i++) {
-            if (arr[i] > memo[len]) {
-                memo[++len] = arr[i];
+            if (A[i] > memo[len]) {
+                memo[++len] = A[i];
             } else {
-                idx = binarySearch(0, len, arr[i]);
-                memo[idx] = arr[i];
+                idx = binarySearch(0, len, A[i]);
+                memo[idx] = A[i];
             }
         }
 
         System.out.println(len);
+
+        br.close();
     }
 
     private static int binarySearch(int left, int right, int target) {
         while (left < right) {
-            int mid = left + (right-left) / 2;
-            if (memo[mid] < target) {
-                left = mid+1;
-            } else right = mid;
+            int mid = (left + right) / 2;
+
+            if (memo[mid] < target) left = mid+1;
+            else right = mid;
         }
+
         return right;
     }
 }
