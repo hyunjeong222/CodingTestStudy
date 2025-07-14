c76fe0e46afc40510df858a58f5918937720dea2
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-07-14 15:02:36 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 72 ms, Memory: 12004 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -13,23 +13,25 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        Stack<Integer> stack = new Stack<>();
-        int now = 1;
+
+        Stack<Integer> stack = new Stack<>(); // 한 명씩만 설 수 있는 공간
+        int now = 1; // 간식을 받을 수 있는 현재 순서
         for (int i = 0; i < n; i++) {
-            if (arr[i] == now) {
+            if (arr[i] == now) { // 간식을 받는 순서라면
                 now++;
+                // 한 명씩만 설 수 있는 공간에서 순서대로 받을 수 있는지 계속 체크
                 while (!stack.isEmpty()) {
                     if (stack.peek() == now) {
                         stack.pop();
                         now++;
-                    } else break;
+                    } else break; // 순서대로가 아니면 더 이상 체크 X
                 }
-            } else {
-                stack.push(arr[i]);
+            } else { // 간식을 받을 수 있는 순서가 아니라면
+                stack.push(arr[i]); // 스택에서 대기
             }
         }
 
+        // 스택이 비어있다면 순서대로 간식을 받을 수 있음
         System.out.println(stack.isEmpty() ? "Nice" : "Sad");
 
         br.close();
