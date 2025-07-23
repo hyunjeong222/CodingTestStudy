46d80bc37f87aabdc733059c954eecf97827fe69
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-07-23 18:19:13 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 296 ms, Memory: 157228 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -7,41 +7,37 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        boolean flag = false; // 조건에 만족하는 수를 찾았는지
 
-        n -= 1; // n보다 크거나 같고 -> n부터 시작
-
-        while (!flag) {
-            n += 1;
-
-            String num = String.valueOf(n);
-            String reversed = new StringBuilder(num).reverse().toString();
-
-            boolean isPrime = true; // 소수 여부
-
-            if (num.equals(reversed)) { // 팰린드롬인 경우
-                // 소수 판별
-
-                if (n == 1) { // 1은 소수 X
-                    isPrime = false;
-                }
-
-                // 소수 판별
-                for (int i = 2; i <= Math.sqrt(n); i++) {
-                    if (n % i == 0) { // 소수 X
-                        isPrime = false;
-                        break;
-                    }
-                }
-
-                if (isPrime) { // 소수가 맞다면
-                    flag = true; // 팰린드롬 & 소수 발견
-                }
+        while (true) {
+            if (isPalindrome(n) && isPrime(n)) {
+                System.out.println(n);
+                break;
             }
+
+            n += 1;
         }
 
-        System.out.println(n);
-
         br.close();
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) return false; // 0, 1은 소수 X
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isPalindrome(int n) {
+        String num = String.valueOf(n);
+        String reversed = new StringBuilder(num).reverse().toString();
+
+        if (num.equals(reversed)) {
+            return true;
+        }
+
+        return false;
     }
 }
