41defb614817c2d32d54d7bea8e9e3f4773b01f4
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-07-23 17:37:47 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 320 ms, Memory: 172612 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -7,29 +7,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        boolean flag = false; // 조건에 만족하는 수를 찾았는지
 
-        boolean[] arr = new boolean[10000001];
-        List<Integer> list = new ArrayList<>();
+        n -= 1; // n보다 크거나 같고 -> n부터 시작
 
-        for (int i = 2; i < 10000001; i++) {
-            if (i >= n && !arr[i]) {
-                list.add(i);
-            }
+        while (!flag) {
+            n += 1;
 
-            for (int j = i+i; j < 10000001; j+=i) {
-                arr[j] = true;
-            }
-        }
-
-        for (int i : list) {
-            String num = String.valueOf(i);
+            String num = String.valueOf(n);
             String reversed = new StringBuilder(num).reverse().toString();
 
-            if (num.equals(reversed)) {
-                System.out.println(num);
-                return;
+            boolean isPrime = true; // 소수 여부
+
+            if (num.equals(reversed)) { // 팰린드롬인 경우
+                // 소수 판별
+
+                if (n == 1) { // 1은 소수 X
+                    isPrime = false;
+                }
+
+                // 소수 판별
+                for (int i = 2; i <= Math.sqrt(n); i++) {
+                    if (n % i == 0) { // 소수 X
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) { // 소수가 맞다면
+                    flag = true; // 팰린드롬 & 소수 발견
+                }
             }
         }
+
+        System.out.println(n);
 
         br.close();
     }
