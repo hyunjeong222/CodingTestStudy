b6b34091b97cd46b6866c27e1471f9dd163abc16
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-07-23 16:39:49 +0900

    [Silver II] Title: 초콜릿 식사, Time: 92 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = 1;
+        int count = 0;
+        
+        while(n<k) n= n*2;
+        
+        StringBuilder sb= new StringBuilder();
+        sb.append(n).append(" ");
+        
+        while(k>0){
+            if(k>=n){
+                k-=n;
+            }else{
+                n/=2;
+                count++;
+            }
+        }
+        
+        sb.append(count);
+        System.out.println(sb);
+    }
+}
