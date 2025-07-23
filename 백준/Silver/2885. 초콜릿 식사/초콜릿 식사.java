48ca0966b8342497453b9c69a3c4cf035b3a0323
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-07-23 17:01:01 +0900

    [Silver II] Title: 초콜릿 식사, Time: 72 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
@@ -1,27 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt();
-        int n = 1;
-        int count = 0;
-        
-        while(n<k) n= n*2;
-        
-        StringBuilder sb= new StringBuilder();
-        sb.append(n).append(" ");
-        
-        while(k>0){
-            if(k>=n){
-                k-=n;
-            }else{
-                n/=2;
-                count++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine()); // 상근이가 먹어야 하는 초콜릿 갯수
+
+        int chocoSize = 1;
+        while(chocoSize < k) {
+            chocoSize *= 2; // 초콜릿 크기는 항상 2의 제곱의 형태
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // 현재 조각과 상근이가 먹어야 하는 초콜릿 갯수가 일치
+        if (chocoSize == k) {
+            sb.append(chocoSize).append(" ").append(0);
+            System.out.println(sb.toString());
+            return;
+        }
+
+        int cuttingCnt = 0; // 쪼개는 횟수
+        int piece = chocoSize;
+        while (k > 0) { // 상근이가 모든 초콜릿 조각을 모을 때까지
+            piece /= 2; // 항상 가운데로만 쪼개짐
+            cuttingCnt++;
+
+            if (piece <= k) { // 상근이가 현재 조각을 가져갈 수 있으면
+                k -= piece; // 가져감
             }
         }
-        
-        sb.append(count);
-        System.out.println(sb);
+
+        sb.append(chocoSize).append(" ").append(cuttingCnt);
+        System.out.println(sb.toString());
+
+        br.close();
     }
 }
