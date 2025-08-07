6fbf83f0f1c71fb5a3a646bbb4234a64264b96b1
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-08-07 12:08:39 +0900

    [Gold I] Title: 냅색문제, Time: 168 ms, Memory: 17220 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.java
+++ 백준/Gold/1450. 냅색문제/냅색문제.java
@@ -7,33 +7,30 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n, c;
+    static int[] arr;
+    static ArrayList<Integer> left, right;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken()); // 물건의 개수
         c = Integer.parseInt(st.nextToken()); // 가방에 넣을 수 있는 무게
 
-        ArrayList<Integer> bag1 = new ArrayList<>();
-        ArrayList<Integer> bag2 = new ArrayList<>();
-
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            if (i < n/2) bag1.add(Integer.parseInt(st.nextToken()));
-            else bag2.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        ArrayList<Integer> sum1 = new ArrayList<>();
-        ArrayList<Integer> sum2 = new ArrayList<>();
-
-        dfs(0, 0, bag1, sum1);
-        dfs(0, 0, bag2, sum2);
-
-        Collections.sort(sum2);
+        left = new ArrayList<>();
+        right = new ArrayList<>();
+        comb(left, 0, n/2, 0);
+        comb(right, n/2, n, 0);
+        Collections.sort(right);
 
         int ans = 0;
-        for (int i = 0; i < sum1.size(); i++) {
-            int searchValue = c - sum1.get(i);
-            ans += binarySearch(sum2, searchValue)+1;
+        for (int i = 0; i < left.size(); i++) {
+            int searchValue = c - left.get(i);
+            ans += binarySearch(right, searchValue)+1;
         }
 
         System.out.println(ans);
@@ -58,17 +55,17 @@ public class Main {
         return right;
     }
 
-    private static void dfs(int idx, int sum, ArrayList<Integer> bag, ArrayList<Integer> ans) {
+    private static void comb(ArrayList<Integer> list, int start, int end, int sum) {
         if (sum > c) return; // 탈출 조건
 
-        if (idx == bag.size()) {
-            ans.add(sum);
+        if (start == end) {
+            list.add(sum);
             return;
         }
 
         // 물건을 넣는 경우
-        dfs(idx+1, sum+bag.get(idx), bag, ans);
+        comb(list, start+1, end, sum+arr[start]);
         // 물건을 넣지 않는 경우
-        dfs(idx+1, sum, bag, ans);
+        comb(list, start+1, end, sum);
     }
 }
