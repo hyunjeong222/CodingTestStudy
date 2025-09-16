6612288c31ea56f9f77ca34a29cc2612e8adeec7
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-09-16 17:20:42 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 72 ms, Memory: 11860 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -4,10 +4,14 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static public class Player {
+    static public class Player implements Comparable<Player> {
         int level; String name;
         public Player (int level, String name) {
             this.level = level; this.name = name;
+        }
+        @Override
+        public int compareTo(Player o) {
+            return this.name.compareTo(o.name);
         }
     }
     static public class Room {
@@ -25,33 +29,36 @@ public class Main {
         ArrayList<Room> rooms = new ArrayList<>();
         for (int i = 0; i < p; i++) {
             st = new StringTokenizer(br.readLine());
-            int l = Integer.parseInt(st.nextToken());
-            String n = st.nextToken();
+            int l = Integer.parseInt(st.nextToken()); // 플레이어 레벨
+            String n = st.nextToken(); // 플레이어 이름
 
             boolean enter = false;
             Room r = null;
-
             for (int j = 0; j < rooms.size(); j++) {
                 r = rooms.get(j);
-                if (r.player.size() >= m) { // 정원 초과
+                if (r.player.size() >= m) {
                     continue;
                 }
-                if (Math.abs(r.level - l) <= 10) {
+                if (Math.abs(r.level-l) <= 10) {
                     r.player.add(new Player(l, n));
                     enter = true;
                     break;
                 }
             }
 
-            if (!enter) { // 들어갈 방이 없다면
-                // 새로운 방 만들기
-                rooms.add(new Room(l, new ArrayList<>(Arrays.asList(new Player(l, n)))));
+            if (!enter) { // 매칭이 가능한 방이 없다면
+                ArrayList<Player> player = new ArrayList<>();
+                player.add(new Player(l, n));
+                rooms.add(new Room(l, player));
             }
+        }
+
+        for (Room r : rooms) {
+            Collections.sort(r.player);
         }
 
         StringBuilder sb = new StringBuilder();
         for (Room r : rooms) {
-            r.player.sort((a, b) -> a.name.compareTo(b.name));
             if (r.player.size() >= m) {
                 sb.append("Started!").append("\n");
             } else {
