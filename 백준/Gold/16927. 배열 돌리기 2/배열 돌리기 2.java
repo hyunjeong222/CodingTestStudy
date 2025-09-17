289e4ba798139ff6fd6473aa0cfc3e8367d6b4ae
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-09-17 17:49:53 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 484 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class Main {
     static int n, m, r;
     static int[][] map;
+    static int[] dx = {0, 1, 0, -1}; // ← ↑ → ↓
+    static int[] dy = {1, 0, -1, 0};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,24 +22,20 @@ public class Main {
             }
         }
 
-        int N = n;
-        int M = m;
-
         int size = Math.min(n, m)/2; // 공간 크기
         int nN = n;
         int mM = m;
         for (int i = 0; i < size; i++) {
             rotate(i, (2*nN)+(2*mM)-4);
-            n -= 1;
-            m -= 1;
             nN -= 2;
             mM -= 2;
         }
         // System.out.println(Arrays.deepToString(map));
 
+        // print
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 sb.append(map[i][j]).append(" ");
             }
             sb.append("\n");
@@ -51,18 +49,22 @@ public class Main {
     private static void rotate(int start, int len) {
         int repeat = r % len; // 회전 횟수
         for (int i = 0; i < repeat; i++) {
-            int tmp = map[start][start];
-            for (int j = start; j < m-1; j++) { // ←
-                map[start][j] = map[start][j+1];
-            }
-            for (int j = start; j < n-1; j++) { // ↑
-                map[j][m-1] = map[j+1][m-1];
-            }
-            for (int j = m-1; j > start; j--) { // →
-                map[n-1][j] = map[n-1][j-1];
-            }
-            for (int j = n-1; j > start; j--) { // ↓
-                map[j][start] = map[j-1][start];
+            int tmp = map[start][start]; // 마지막에 넣을 값
+
+            int idx = 0; // 다음 값 탐색할 인덱스
+
+            int x = start;
+            int y = start;
+
+            while (idx < 4) { // 네 방향(0~3)
+                int nx = x + dx[idx];
+                int ny = y + dy[idx];
+
+                if (nx >= start && ny >= start && nx < n-start && ny < m-start) {
+                    // 경계 안 → 같은 방향으로 계속 이동
+                    map[x][y] = map[nx][ny];
+                    x = nx; y = ny;
+                } else idx++; // 경계 밖 → 방향 바꾸기
             }
             map[start+1][start] = tmp;
         }
