43626414c2b3d51493c06621459ae49080393d19
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-09-30 21:11:40 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 124 ms, Memory: 20416 KB -BaekjoonHub

---
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static final int MOD = 1_000_000_007;
+    static long[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        dp = new long[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(func(n, m)%MOD);
+
+        br.close();
+    }
+
+    private static long func(int n, int m) {
+        if (n == 1 && m == 1) return 1; // (1, 1) -> (1, 1) 경우의 수 1
+        if (n == 0 || m == 0) return 0; //
+
+        if (dp[n][m] != -1) return dp[n][m];
+        long ans = (func(n ,m-1)+func(n-1, m)+func(n-1, m-1)) % MOD;
+        dp[n][m] = ans;
+
+        return ans;
+    }
+}
