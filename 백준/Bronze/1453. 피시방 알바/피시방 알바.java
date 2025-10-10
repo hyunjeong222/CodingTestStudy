57db6a2b4e1bbd284c5bf4bfffc75630fe57643f
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-10-10 14:34:52 +0900

    [Bronze II] Title: 피시방 알바, Time: 64 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Bronze/1453. 피시방 알바/피시방 알바.java
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.java
@@ -1,22 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[] checked = new boolean[101];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (checked[num]) cnt++;
+            checked[num] = true;
+        }
 
-	public static void main(String[] args) {
-		boolean[] check = new boolean[101];
-		
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		
-		int cnt = 0;
-		for(int i=0;i<size;i++) {
-			int tmp = sc.nextInt();
-			
-			if(check[tmp]==true)
-				cnt++;
-			else
-				check[tmp] = true;
-		}
-		System.out.println(cnt);
-	}
+        System.out.println(cnt);
+
+        br.close();
+    }
 }
