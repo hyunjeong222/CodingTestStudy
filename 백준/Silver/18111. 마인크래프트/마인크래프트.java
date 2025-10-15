83c945d605655caf35c697935a7b2fcdaa17628c
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-10-15 16:36:48 +0900

    [Silver II] Title: 마인크래프트, Time: 548 ms, Memory: 34588 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -7,6 +7,7 @@ public class Main {
     static int n, m, b;
     static int[][] map;
     static int min = 257, max = 0;
+    // 땅을 고르는 최소 시간, 땅의 높이
     static int minTime = Integer.MAX_VALUE, height = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,42 +27,40 @@ public class Main {
                 max = Math.max(max, map[i][j]);
             }
         }
-        // System.out.println(min);
-        // System.out.println(max);
 
-        for (int i = max; i >= min; i--) {
-            blockCheck(i);
+        // 블록을 하나 꺼내 쌓는게
+        // 시간이 덜 걸림 (1초)
+        for (int h = max; h >= min; h--) {
+            // 땅 고르기
+            blockCheck(h);
         }
 
         sb.append(minTime).append(" ").append(height).append("\n");
-
         System.out.println(sb.toString());
 
         br.close();
     }
 
     private static void blockCheck(int h) {
-        int nowTime = 0;
-        int blockCnt = b;
+        int t = 0, block = b;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                // 최소 시간보다 커지면 종료
-                if (nowTime > minTime) return;
+                if (t > minTime) return;
 
-                if (map[i][j] > h) { // 1번 작업 : 땅 파기
-                    int sub = map[i][j]-h;
-                    nowTime += 2*sub;
-                    blockCnt += sub;
-                } else if (map[i][j] < h) { // 2번 작업 : 땅 놓기
-                    int sub = h-map[i][j];
-                    nowTime += sub;
-                    blockCnt -= sub;
+                if (map[i][j] > h) { // 작업 1 : 블록 제거 2초
+                    int sub = map[i][j]-h; // 제거한 블록
+                    t += sub*2;
+                    block += sub; // 제거한 블록 인벤토리 저장
+                } else if (map[i][j] < h) { // 작업 2 : 블록 쌓기 1초
+                    int sub = h-map[i][j]; // 쌓아야 하는 블록
+                    t += sub;
+                    block -= sub; // 인벤토리에서 블록 꺼내서 쌓기
                 }
             }
         }
 
-        if (blockCnt >= 0 && minTime > nowTime) {
-            minTime = nowTime;
+        if (block >= 0 && minTime > t) {
+            minTime = t;
             height = h;
         }
     }
