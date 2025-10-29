e6b6b6a9dd6be1f7d8cbe58d3450398f9c44c81d
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-10-29 18:22:26 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 68 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -1,17 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < x; i++) {
-			while (sc.hasNext()) {
-				StringBuilder sb = new StringBuilder(sc.next());
-				System.out.println(sb.reverse());
-			}
-		}
-		sc.close();
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder inputLine = new StringBuilder(br.readLine());
+            sb.append(inputLine.reverse()).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
 }
