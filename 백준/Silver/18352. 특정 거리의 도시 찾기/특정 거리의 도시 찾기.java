e8396072362a380934db20a034c75a526e3764a7
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-11-14 14:06:08 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1296 ms, Memory: 346168 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,30 +1,29 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     static int n, m, k, x;
-    static ArrayList<ArrayList<pos>> list;
+    static ArrayList<ArrayList<Pos>> list;
     static int[] dist;
-    static boolean[] checked;
-    static public class pos implements Comparable<pos>{
-        int end; int dist;
-
-        public pos (int end, int dist) {
-            this.end = end;
-            this.dist = dist;
+    // static boolean[] checked;
+    static public class Pos implements Comparable<Pos>{
+        int to; int cost;
+        public Pos (int to, int cost) {
+            this.to = to;
+            this.cost = cost;
         }
-
         @Override
-        public int compareTo(pos o) {
-            return this.dist - o.dist;
+        public int compareTo(Pos o) {
+            return this.cost - o.cost;
         }
     }
+    static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken()); // 도시의 개수
         m = Integer.parseInt(st.nextToken()); // 도로의 개수
@@ -37,14 +36,14 @@ public class Main {
         }
 
         dist = new int[n+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(dist, INF);
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken()); // 시작점
             int b = Integer.parseInt(st.nextToken()); // 목적지
 
-            list.get(a).add(new pos(b, 1));
+            list.get(a).add(new Pos(b, 1));
         }
 
         dijkstra(x);
@@ -58,24 +57,29 @@ public class Main {
         if (sb.length() == 0) sb.append(-1).append("\n");
 
         System.out.println(sb.toString());
+
         br.close();
     }
 
     private static void dijkstra(int start) {
-        PriorityQueue<pos> pq = new PriorityQueue<>();
-        checked = new boolean[n+1];
-        pq.offer(new pos(start, 0));
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
+        // checked = new boolean[n+1];
+
+        pq.offer(new Pos(start, 0));
         dist[start] = 0;
 
         while (!pq.isEmpty()) {
-            pos now = pq.poll();
-            int num = now.end;
-            checked[num] = true;
+            Pos now = pq.poll();
+            int node = now.to;
+            // checked[node] = true;
 
-            for (pos next : list.get(num)) {
-                if (!checked[next.end] && dist[next.end] > dist[num] + next.dist) {
-                    dist[next.end] = dist[num] + next.dist;
-                    pq.offer(new pos(next.end, dist[next.end]));
+            if (now.cost > dist[now.to]) continue;
+
+            for (Pos next : list.get(node)) {
+                // !checked[next.to] &&
+                if (dist[next.to] > dist[node] + next.cost) {
+                    dist[next.to] = dist[node] + next.cost;
+                    pq.offer(new Pos(next.to, dist[next.to]));
                 }
             }
         }
