87667b87b9a7d7c02025510c94821f13040fbd8d
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-11-14 14:32:29 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 608 ms, Memory: 68928 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,35 +1,28 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int n, e;
-    static int u, v;
-    static ArrayList<ArrayList<pos>> list;
+    static int n, e, u, v;
+    static PriorityQueue<Pos> pq;
+    static ArrayList<ArrayList<Pos>> list;
     static int[] dist;
-    static boolean[] checked;
-    static PriorityQueue<pos> pq;
-
-    static public class pos implements Comparable<pos> {
-        int end;
-        int dist;
-
-        public pos(int end, int dist) {
-            this.end = end;
-            this.dist = dist;
+    static public class Pos implements Comparable<Pos>{
+        int to; int cost;
+        public Pos (int to, int cost) {
+            this.to = to;
+            this.cost = cost;
         }
-
         @Override
-        public int compareTo(pos o) {
-            return this.dist - o.dist;
+        public int compareTo(Pos o) {
+            return this.cost - o.cost;
         }
     }
-    static int INF = 200000000;
+    static final int INF = 200000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken()); // 정점의 개수
         e = Integer.parseInt(st.nextToken()); // 간선의 개수
@@ -41,35 +34,31 @@ public class Main {
 
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            // a -> b 양방향
             int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            // 그 거리가 c
+            int b = Integer.parseInt(st.nextToken()); // 가중치
             int c = Integer.parseInt(st.nextToken());
 
-            list.get(a).add(new pos(b, c));
-            list.get(b).add(new pos(a, c));
+            list.get(a).add(new Pos(b, c));
+            list.get(b).add(new Pos(a, c));
         }
 
-        // 반드시 거쳐야 하는 두 개의 서로 다른 정점
+        // 반드시 거쳐야 하는 두 개의 서로 다른 정점 번호
         st = new StringTokenizer(br.readLine());
         u = Integer.parseInt(st.nextToken());
         v = Integer.parseInt(st.nextToken());
 
-        int distA = 0; // u -> v일때 최단거리
+        int distA = 0; // u -> v일 때 최단거리
         distA += dijkstra(1, u);
         distA += dijkstra(u, v);
         distA += dijkstra(v, n);
 
-        int distB = 0; // v -> u일때 최단거리
+        int distB = 0; // v -> u일 때 최단거리
         distB += dijkstra(1, v);
         distB += dijkstra(v, u);
         distB += dijkstra(u, n);
 
-        int ans = (distA >= INF && distB >= INF) ? -1 : Math.min(distA, distB);
-        bw.append(ans + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println((distA >= INF && distB >= INF) ? -1 : Math.min(distA, distB));
+
         br.close();
     }
 
@@ -78,21 +67,23 @@ public class Main {
         Arrays.fill(dist, INF);
 
         pq = new PriorityQueue<>();
-        pq.offer(new pos(start, 0));
-        checked = new boolean[n+1];
+        pq.offer(new Pos(start, 0));
+
         dist[start] = 0;
 
         while (!pq.isEmpty()) {
-            pos now = pq.poll();
-            checked[now.end] = true;
+            Pos now = pq.poll();
 
-            for (pos next : list.get(now.end)) {
-                if (!checked[next.end] && dist[next.end] > dist[now.end] + next.dist) {
-                    dist[next.end] = dist[now.end] + next.dist;
-                    pq.offer(new pos(next.end, dist[next.end]));
+            if (now.cost > dist[now.to]) continue;
+
+            for (Pos next : list.get(now.to)) {
+                if (dist[next.to] > dist[now.to] + next.cost) {
+                    dist[next.to] = dist[now.to] + next.cost;
+                    pq.offer(new Pos(next.to, dist[next.to]));
                 }
             }
         }
+
         return dist[end];
     }
 }
