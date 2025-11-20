d663305a549874ed3f339b0ed22eb2e748c6f7d6
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-11-20 14:42:36 +0900

    [Silver V] Title: 돌 게임 2, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] size, memo;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine()); // 돌의 개수
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println((n%2==0) ? "SK" : "CY");
+
+
+        br.close();
+    }
+}
