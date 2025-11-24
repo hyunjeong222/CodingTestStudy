69828e9de9cd2403d4a87e8bf8eff52ed35c1771
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-11-24 16:15:43 +0900

    [Silver III] Title: 블로그, Time: 256 ms, Memory: 38680 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -5,43 +5,45 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken()); // 블로그를 시작하고 지난 일수
-        int x = Integer.parseInt(st.nextToken()); // x일 동안
+        int n = Integer.parseInt(st.nextToken()); // 블로그 운영일 수
+        int x = Integer.parseInt(st.nextToken()); // 관찰할 기간 길이
 
         st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) { // 1일차부터 n일차까지 하루 방문자 수 입력
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); // 하루 방문자 수 입력
         }
 
-        int sum = 0;
-        int day = 1; // x일 동안 가장 많이 들어온 기간
-        for (int i = 0; i < x; i++) sum += arr[i]; // x일마다 방문자수의 합
+        int sum = 0; // 현재 X일 구간의 방문자 합
+        int cnt = 1; // 최대 방문자 수를 기록한 기간의 개수
+        
+        // 1일부터 x일까지 방문자수의 합
+        for (int i = 0; i < x; i++) sum += arr[i];
 
-        int max = sum; // 최대 방문자수
+        int max = sum; // 현재까지의 최대 방문자 수
+        
         for (int i = x; i < n; i++) {
-            sum -= arr[i-x]; // 첫번째 제외
-            sum += arr[i];
+            sum -= arr[i-x]; // 빠지는 값 제거
+            sum += arr[i]; // 새로 포함되는 값 추가
 
-            if (max < sum) { // 최댓값 갱신
-                day = 0; // 최댓값 갱신되면 날짜 초기화
-                day++;
+            if (sum > max) { // 더 큰 값 발견 시
                 max = sum;
-            } else if (max == sum) { // 같은 기간이면
-                day++;
+                cnt = 1; // 기간 개수 초기화 후 1로 설정
+            } else if (sum == max) { 
+                cnt++; // 동일한 최대값인 기간 발견
             }
         }
 
-        if (max == 0) { // 최대 방문자 수가 0명
-            bw.append("SAD" + "\n");
-            bw.flush();
-            return;
+        if (max == 0) { // 모든 구간의 방문자 수가 0인 경우
+            bw.write("SAD\n");
+        } else {
+            bw.write(max + "\n" + cnt);
         }
-
-        bw.append(max + "\n" + day);
+        
         bw.flush();
+        
         bw.close();
         br.close();
     }
