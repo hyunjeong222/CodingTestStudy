9a6b0a26234c22fc72ec3ad5d144d14cf47952d1
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-11-29 23:33:48 +0900

    [Bronze V] Title: AI 선도대학, Time: 68 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine()); // 문자열 길이
+
+        System.out.println(n-2024);
+
+        br.close();
+    }
+}
