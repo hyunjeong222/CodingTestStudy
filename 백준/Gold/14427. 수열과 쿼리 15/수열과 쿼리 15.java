9b9c7e776f5ec3678b9b67e95cdc3eb505405af2
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-12-04 16:20:14 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 512 ms, Memory: 52616 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
@@ -4,54 +4,70 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static public class Pos implements Comparable<Pos> {
-        int idx; long num;
-        public Pos(int idx, long num) {
-            this.idx = idx; this.num = num;
-        }
-        @Override
-        public int compareTo(Pos o) {
-            if (this.num == o.num) return this.idx - o.idx;
-            return Long.compare(this.num, o.num);
-        }
-    }
+    static int n;
+    static int[] arr, tree;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        PriorityQueue<Pos> pq = new PriorityQueue<>();
-
         int n = Integer.parseInt(br.readLine()); // 수열의 크기
-        StringTokenizer st = new StringTokenizer(br.readLine()); // 최신값 저장용 배열
-        long[] arr = new long[n+1];
+
+        arr = new int[n+1];
+        tree = new int[n*4];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
-            // 인덱스 넘버, 숫자
-            pq.offer(new Pos(i, arr[i]));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        init(1, n, 1);
 
         int m = Integer.parseInt(br.readLine()); // 쿼리의 개수
         while (m --> 0) {
             st = new StringTokenizer(br.readLine());
-            int cmd = Integer.parseInt(st.nextToken());
+            int cmd = Integer.parseInt(st.nextToken()); // 명령
 
             if (cmd == 1) {
+                // Ai를 v로 바꿈
                 int i = Integer.parseInt(st.nextToken());
-                long v = Long.parseLong(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
 
-                arr[i] = v; // 최신값 저장
-                pq.add(new Pos(i, v));
-            } else { // 1 : 크기가 가장 작은 값의 인덱스를 출력
-                while (!pq.isEmpty() && pq.peek().num != arr[pq.peek().idx]) {
-                    pq.poll();
-                }
-
-                sb.append(pq.peek().idx).append("\n");
+                arr[i] = v;
+                
+                update(1, n, 1, i);
+            } else { // 2 : 가장 작은 값의 인덱스 출력, 그러한 값이 여러개인 경우 인덱스가 작은 것 출력
+                sb.append(tree[1]).append("\n");
             }
         }
 
         System.out.println(sb.toString());
 
         br.close();
+    }
+
+    private static int init(int start, int end, int node) {
+        if (start == end) {
+            return tree[node] = start;
+        }
+
+        int mid = (start+end)/2;
+
+        return tree[node] = minIdx(init(start, mid, node*2), init(mid+1, end, node*2+1));
+    }
+
+    private static int minIdx(int x, int y) {
+        if (arr[x] == arr[y]) {
+            return Math.min(x, y);
+        }
+
+        return arr[x] < arr[y] ? x : y;
+    }
+
+    private static int update(int start, int end, int node, int idx) {
+        if (start > idx || end < idx) return tree[node];
+
+        if (start == end) return tree[node] = idx;
+
+        int mid = (start+end)/2;
+        return tree[node] = minIdx(update(start, mid, node*2, idx), update(mid+1, end, node*2+1, idx));
     }
 }
