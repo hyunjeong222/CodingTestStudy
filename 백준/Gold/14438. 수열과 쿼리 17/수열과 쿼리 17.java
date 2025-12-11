11db8240113de860ff68821ebc0f563c973ee4a3
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-12-11 16:24:06 +0900

    [Gold I] Title: 수열과 쿼리 17, Time: 524 ms, Memory: 64024 KB -BaekjoonHub

---
+++ 백준/Gold/14438. 수열과 쿼리 17/수열과 쿼리 17.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr, tree;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine()); // 수열의 크기
+        arr = new int[n+1];
+        // arr[0] = Integer.MAX_VALUE;
+        tree = new int[n*4];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        init(1, n, 1);
+
+        m = Integer.parseInt(br.readLine()); // 쿼리의 개수
+        while (m --> 0) {
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+
+            if (cmd == 1) { // Ai를 v로 바꾸기
+                int i = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+
+                arr[i] = v;
+                update(1, n, 1, i);
+            } else { // Ai~Aj에서 크기가 가장 작은 값을 출력
+                int i = Integer.parseInt(st.nextToken());
+                int j = Integer.parseInt(st.nextToken());
+
+                sb.append(getMin(1, n, 1, i, j)).append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+
+    private static int getMin(int start, int end, int node, int left, int right) {
+        if (right < start || end < left) return Integer.MAX_VALUE;
+        if (left <= start && right >= end) return tree[node];
+
+        int mid = (start+end)/2;
+        return Math.min(getMin(start, mid, node*2, left, right), getMin(mid+1, end, node*2+1, left, right));
+    }
+
+    private static int update(int start, int end, int node, int idx) {
+        if (start > idx || end < idx) return tree[node];
+        if (start == end) return tree[node] = arr[idx];
+
+        int mid = (start+end)/2;
+        return tree[node] = Math.min(update(start, mid, node*2, idx), update(mid+1, end, node*2+1, idx));
+    }
+
+    private static int init(int start, int end, int node) {
+        if (start == end) {
+            return tree[node] = arr[start];
+        }
+
+        int mid = (start+end)/2;
+        return tree[node] = Math.min(init(start, mid, node*2), init(mid+1, end, node*2+1));
+    }
+}
