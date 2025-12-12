4f92356149dc5e17cd8461957c0b077a495757dd
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-12-12 14:54:26 +0900

    [Gold I] Title: 최솟값, Time: 436 ms, Memory: 54448 KB -BaekjoonHub

---
+++ 백준/Gold/10868. 최솟값/최솟값.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr, tree;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 수열의 크기
+        m = Integer.parseInt(st.nextToken()); // 쿼리의 개수
+        arr = new int[n+1];
+        tree = new int[n*4];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        init(1, n, 1);
+
+        while (m --> 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(getMin(1, n, 1, a, b)).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+
+    private static int getMin(int start, int end, int node, int left, int right) {
+        if (right < start || end < left) return Integer.MAX_VALUE;
+        if (left <= start && right >= end) return tree[node];
+
+        int mid = (start+end)/2;
+        return Math.min(getMin(start, mid, node*2, left, right), getMin(mid+1, end, node*2+1, left, right));
+    }
+
+    private static int init(int start, int end, int node) {
+        if (start == end) {
+            return tree[node] = arr[start];
+        }
+
+        int mid = (start+end)/2;
+        return tree[node] = Math.min(init(start, mid, node*2), init(mid+1, end, node*2+1));
+    }
+}
