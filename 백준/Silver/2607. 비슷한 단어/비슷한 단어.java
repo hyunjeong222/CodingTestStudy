854e4abe7eebc79334f9f1f43018270920cf421b
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2025-12-16 21:03:37 +0900

    [Silver II] Title: 비슷한 단어, Time: 64 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -10,35 +10,36 @@ public class Main {
 
         // StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(br.readLine()); // 단어의 개수
-        String firstStr = br.readLine();
-
-        int ans = 0;
+        int ans = 0; // 비슷한 단어의 개수
+        String first = br.readLine();
         for (int i = 0; i < n-1; i++) {
             String str = br.readLine();
 
-            int  cnt = 0; // 같은 단어 개수
-            int[] words = new int[26];
-            for (int j = 0; j < firstStr.length(); j++) {
-                words[firstStr.charAt(j)-'A']++;
+            int cnt = 0; // 같은 문자 개수
+
+            int[] alpha = new int[26];
+            for (int j = 0; j < first.length(); j++) {
+                alpha[first.charAt(j)-'A']++;
             }
 
             for (int j = 0; j < str.length(); j++) {
-                if (words[str.charAt(j)-'A'] > 0) {
+                if (alpha[str.charAt(j)-'A'] > 0) {
                     cnt++;
-                    words[str.charAt(j)-'A']--;
+                    alpha[str.charAt(j)-'A']--;
                 }
             }
 
-            // 같은 경우 or 하나만 다른 경우
-            if (firstStr.length() == str.length() && (firstStr.length() == cnt || firstStr.length()-1 == cnt)) ans++;
-            // 비교 문자열이 짧은 경우 - 제거
-            else if (firstStr.length() == str.length()-1 && str.length()-1 == cnt) ans++;
-            // 비교 문자열이 긴 경우 - 추가
-            else if (firstStr.length() == str.length()+1 && str.length() == cnt) ans++;
+            // 기준 문자열과 비교 문자열의 길이가 같다면
+            // 모든 문자의 종류가 같거나 하나의 문자를 다른 문자로 바꾸거나
+            if (first.length() == str.length() && (first.length() == cnt || first.length()-1 == cnt)) ans++;
+            // 기준 문자열의 길이가 짧다면
+            else if (first.length() == str.length()-1 && str.length()-1 == cnt) ans++;
+            // 기준 문자열의 길이가 길다면
+            else if (first.length() == str.length()+1 && str.length() == cnt) ans++;
         }
 
         System.out.println(ans);
-
+        
         br.close();
     }
 }
