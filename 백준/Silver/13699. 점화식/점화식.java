4dd1e36c550422b830c922cd9b6d31145f19c43f
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-01-02 12:09:56 +0900

    [Silver IV] Title: 점화식, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb = new StringBuilder();
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[36];
+        dp[0] = 1; dp[1] = 1;
+        for (int i = 2; i <= 35; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += (dp[j]*dp[i-j-1]);
+            }
+        }
+
+        System.out.println(dp[n]);
+
+        br.close();
+    }
+}
