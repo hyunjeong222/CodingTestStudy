927e6b6155b03fb588f701362040399c7ab3e6b2
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-01-03 21:20:09 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 60 ms, Memory: 11320 KB -BaekjoonHub

---
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        long[] dp = new long[70];
+        dp[0] = 1; dp[1] = 1; dp[2] = 2; dp[3] = 4;
+        for (int i = 4; i < 70; i++) {
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]+dp[i-4];
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        while (t --> 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            sb.append(dp[n]).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+}
