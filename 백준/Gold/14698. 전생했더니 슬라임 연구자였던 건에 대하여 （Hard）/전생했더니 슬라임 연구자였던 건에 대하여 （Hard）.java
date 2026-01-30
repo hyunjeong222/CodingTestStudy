069cc323c58831ce22ec5f1e80e9920ba77b72ac
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-01-30 12:39:37 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 824 ms, Memory: 159172 KB -BaekjoonHub

---
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static final long MOD = 1_000_000_007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Long> pq;
+        StringTokenizer st;
+        while (t --> 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            st = new StringTokenizer(br.readLine());
+            pq = new PriorityQueue<>();
+            for (int i = 0; i < n; i++) {
+                long energy = Long.parseLong(st.nextToken());
+                pq.offer(energy);
+            }
+
+            
+            long ans = 1;
+            while (pq.size() > 1) {
+                long fir = pq.poll();
+                long sec = pq.poll();
+                long tmp = fir * sec;
+
+                ans *= tmp % MOD;
+                ans %= MOD;
+
+                pq.offer(tmp);
+            }
+
+            sb.append(ans).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+}
