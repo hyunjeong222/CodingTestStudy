e3fc1f0cff0f3bab58eea004cef5459a2bb02446
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-03-02 20:33:03 +0900

    [Gold IV] Title: 좋은 친구, Time: 264 ms, Memory: 50060 KB -BaekjoonHub

---
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb  = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 학생 수
+        int k = Integer.parseInt(st.nextToken()); // 반 등수의 차이
+
+        Queue<Integer>[] que = new Queue[21]; // 이름 길이별로 학생들의 등수를 저장
+        for (int i = 0; i < 21; i++) {
+            que[i] = new LinkedList<>();
+        }
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            String name = br.readLine();
+            int len = name.length();
+
+            while (!que[len].isEmpty() && que[len].peek() < i-k) {
+                que[len].poll();
+            }
+
+            ans += que[len].size(); // 현재 가능한 친구 수 더하기
+            que[len].add(i); // 현재 학생 추가
+        }
+
+        System.out.println(ans);
+
+        br.close();
+    }
+}
