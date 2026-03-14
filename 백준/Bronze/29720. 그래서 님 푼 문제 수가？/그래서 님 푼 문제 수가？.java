423a5be89244588f5b3341a848fd70e2bdc95b5a
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-03-14 23:55:45 +0900

    [Bronze III] Title: 그래서 님 푼 문제 수가?, Time: 64 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/29720. 그래서 님 푼 문제 수가？/그래서 님 푼 문제 수가？.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
+        int temp=n-m*k;
+        temp= temp < 0 ? 0: temp;
+        bw.write(temp+" "+(n-m*k+m-1));
+        bw.flush();
+
+    }
+}
