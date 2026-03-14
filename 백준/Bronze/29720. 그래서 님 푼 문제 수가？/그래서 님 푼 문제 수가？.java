9a1f23b38a057adff53d5cb30dd250faaf0cb56a
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-03-15 00:09:25 +0900

    [Bronze III] Title: 그래서 님 푼 문제 수가?, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/29720. 그래서 님 푼 문제 수가？/그래서 님 푼 문제 수가？.java
+++ 백준/Bronze/29720. 그래서 님 푼 문제 수가？/그래서 님 푼 문제 수가？.java
@@ -1,18 +1,25 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int n=Integer.parseInt(st.nextToken());
-        int m=Integer.parseInt(st.nextToken());
-        int k=Integer.parseInt(st.nextToken());
-        int temp=n-m*k;
-        temp= temp < 0 ? 0: temp;
-        bw.write(temp+" "+(n-m*k+m-1));
-        bw.flush();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int tmp = n-(m*k);
+        tmp = tmp < 0 ? 0 : tmp;
+
+        sb.append(tmp).append(" ").append(n-(m*k)+m-1).append("\n");
+
+        System.out.println(sb.toString());
+
+        br.close();
     }
 }
