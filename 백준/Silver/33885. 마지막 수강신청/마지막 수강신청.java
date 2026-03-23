84983e12937a7b6e31990941ff7a729f52ecef07
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-03-23 17:21:11 +0900

    [Silver II] Title: 마지막 수강신청, Time: 72 ms, Memory: 11824 KB -BaekjoonHub

--- 백준/Silver/33885. 마지막 수강신청/마지막 수강신청.java
+++ 백준/Silver/33885. 마지막 수강신청/마지막 수강신청.java
@@ -6,9 +6,9 @@ import java.util.*;
 public class Main {
     static int n, m;
     static List<Course> courses = new ArrayList<>();
-    static boolean[][] used = new boolean[5][24]; // 요일(0~4), 시간(0~23)
+    static boolean[][] checked = new boolean[5][24]; // 요일(0~4), 시간(0~23)
     static class Lecture {
-        int day, hour;
+        int day; int hour;
         Lecture (int day, int hour) {
             this.day = day; this.hour = hour;
         }
@@ -16,9 +16,10 @@ public class Main {
     static class Course {
         int credit;
         List<Lecture> lectures = new ArrayList<>();
-        Course(int credit) {
+        Course (int credit) {
             this.credit = credit;
         }
+
     }
     static boolean flag = false;
     public static void main(String[] args) throws IOException {
@@ -32,14 +33,15 @@ public class Main {
         int c, s;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            c = Integer.parseInt(st.nextToken()); // 얻는 학점
-            s = Integer.parseInt(st.nextToken()); // 강의 횟수
+            c = Integer.parseInt(st.nextToken()); // 얻을 수 있는 학점
+            s = Integer.parseInt(st.nextToken()); // 진행되는 강의 횟수
 
-            Course course = new Course(c);
+            Course course = new Course(c); // credit
             for (int j = 0; j < s; j++) {
-                String d = st.nextToken(); // 강의 요일
-                int h = Integer.parseInt(st.nextToken()); // 시작 시간
-                course.lectures.add(new Lecture(dayToInt(d), h));
+                String day = st.nextToken(); // 강의가 열리는 요일
+                int hour = Integer.parseInt(st.nextToken()); // 강의가 열리는 시각
+
+                course.lectures.add(new Lecture(dayToInt(day), hour));
             }
 
             courses.add(course);
@@ -55,51 +57,60 @@ public class Main {
     private static void dfs(int idx, int sum) {
         if (flag) return;
 
+        // m학점 이상 수강 가능
         if (sum >= m) {
             flag = true;
             return;
         }
 
+        // 모든 탐색 종료
         if (idx == n) return;
 
         Course now = courses.get(idx);
-        if (canTake(now)) { // 선택
+
+        // 선택할 때
+        if (canTake(now)) {
             take(now);
             dfs(idx+1, sum+now.credit);
             untake(now);
         }
 
-        dfs(idx+1, sum); // 선택 x
+        // 선택 안 할 때
+        dfs(idx+1, sum);
     }
 
+    // 시간 겹치는지 체크
     private static boolean canTake(Course c) {
-        for (Lecture lec : c.lectures) {
-            if (used[lec.day][lec.hour]) return false;
+        for (Lecture l : c.lectures) {
+            if (checked[l.day][l.hour]) return false;
         }
 
         return true;
     }
 
+    // 시간 표시
     private static void take(Course c) {
-        for (Lecture lec : c.lectures) {
-            used[lec.day][lec.hour] = true;
+        for (Lecture l : c.lectures) {
+            checked[l.day][l.hour] = true;
         }
     }
 
+    // 되돌리기
     private static void untake(Course c) {
-        for (Lecture lec : c.lectures) {
-            used[lec.day][lec.hour] = false;
+        for (Lecture l : c.lectures) {
+            checked[l.day][l.hour] = false;
         }
     }
 
-    private static int dayToInt(String d) {
-        switch (d) {
-            case "MON": return 0;
-            case "TUE": return 1;
-            case "WED": return 2;
-            case "THU": return 3;
-            case "FRI": return 4;
+    private static int dayToInt(String day) {
+        switch (day) {
+            case "MON" : return 0;
+            case "TUE" : return 1;
+            case "WED" : return 2;
+            case "THU" : return 3;
+            case "FRI" : return 4;
         }
+
         return -1;
     }
 }
