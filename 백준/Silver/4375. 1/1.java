0670b8caa895d6bb01598916a0312edbcab3c210
jeong_ii <57525431+hyunjeong222@users.noreply.github.com>
2026-04-27 01:11:36 +0900

    [Silver III] Title: 1, Time: 476 ms, Memory: 141364 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,27 +1,30 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
-    private void solution() throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         String s = "";
-        StringBuilder answer = new StringBuilder();
-        while ((s=br.readLine()) != null) {
+        while ((s = br.readLine()) != null) {
             BigInteger n = new BigInteger(s);
             BigInteger base = BigInteger.ZERO;
-            while(true) {
+
+            while (true) {
                 base = base.multiply(BigInteger.TEN).add(BigInteger.ONE);
                 if (base.mod(n).equals(BigInteger.ZERO)) {
-                    answer.append(base.toString().length()).append('\n');
+                    sb.append(base.toString().length()).append("\n");
                     break;
                 }
             }
         }
-        System.out.print(answer);
-    }
 
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
+        System.out.println(sb.toString());
+
+        br.close();
     }
 }
