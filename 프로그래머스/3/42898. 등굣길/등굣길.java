044a388370ddc99c25d2db1e8cce1df683c6a993
jeong_ii <hyenj629@naver.com>
2026-07-04 00:37:12 +0900

    [level 3] Title: 등굣길, Time: 1.90 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    static final int MOD = 1_000_000_007;
+    
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n+1][m+1];
+        for (int[] i : puddles) {
+            dp[i[1]][i[0]] = -1;
+        }
+
+        dp[1][1] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (dp[i][j] == -1) continue; // 웅덩이
+
+                if (dp[i-1][j] > 0) {
+                    dp[i][j] = (dp[i][j] + dp[i-1][j])%MOD;
+                }
+                if (dp[i][j-1] > 0) {
+                    dp[i][j] = (dp[i][j] + dp[i][j-1])%MOD;
+                }
+            }
+        }
+
+
+        int ans = dp[n][m]; // 채울 수 있는 가장 많은 조각 수
+        
+        return ans;
+    }
+}
